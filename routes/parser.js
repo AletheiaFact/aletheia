fe3d14024c6d65f1fdf71525c3c626a9e98d58a9
Mateus Santos <msantos@wikimedia.org>
2019-01-21 19:23:27 -0200

    Creating prototype of speech HTML parser
    This speech prototype parser reads a HTML file and create a parsed
    object which needs to be stored. The parser is supposed to run when a
    new speech is inserted. The parsed speech is then served to
    alethei-front to be displayed.

---
+++ routes/parser.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const parser = require('../lib/parser');
+
+/**
+ * The main router object
+ */
+const router = require('../lib/util').router();
+
+/**
+ * GET {domain}/candidate
+ */
+router.get('/', (req, res, next) => {
+    
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    let p = new parser();
+    let result = p.parse()
+    res.json(result.object);
+});
+
+module.exports = function(appObj) {
+    return {
+        path: '/testparser',
+        api_version: 1,
+        router
+    };
+};
