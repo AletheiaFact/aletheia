6e80971f649d1d8174d457f2b6916409d30b657c
Eduardo Neves <neves98@outlook.com>
2019-09-28 15:01:04 -0300

    168511489: create all seeders

---
+++ packages/server/infra/database/config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    mongodb: 'mongodb://localhost/Aletheia',
+};
--- packages/server/infra/database/seeds/generateSeeds.js
+++ packages/server/infra/database/seeds/generateSeeds.js
@@ -1,13 +1,21 @@
 'use strict';
 
 const seeder = require('mongoose-seed');
-const Config = require('../../config/config');
+const Config = require('../config');
 
 const fs = require('fs');
 const seeders = [
     {
         'model': 'Personality',
         'documents': JSON.parse(fs.readFileSync('./infra/database/seeds/personality.json', 'utf8'))
+    },
+    {
+        'model': 'Claim',
+        'documents': JSON.parse(fs.readFileSync('./infra/database/seeds/claim.json', 'utf8'))
+    },
+    {
+        'model': 'ClaimReview',
+        'documents': JSON.parse(fs.readFileSync('./infra/database/seeds/claimReview.json', 'utf8'))
     }
 ];
 
@@ -16,10 +24,12 @@ seeder.setLogOutput(false);
 seeder.connect(Config.mongodb, () => {
     // Load Mongoose models
     seeder.loadModels([
-        './app/model/personality'
+        './api/model/personalityModel',
+        './api/model/claimModel',
+        './api/model/claimReviewModel',
     ]);
     // Clear specified collections
-    seeder.clearModels(['Personality', 'Users', 'Groups'], () => {
+    seeder.clearModels(['Personality', 'Claim', 'ClaimReview'], () => {
     // Callback to populate DB once collections have been cleared
         seeder.populateModels(seeders, () => {
             seeder.disconnect();
