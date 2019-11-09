db5329658e5b879f0d7f9a231f838553166453ff
Mateus Santos <msantos@wikimedia.org>
2019-11-09 14:31:55 -0300

    Personality List
    1) Fix Grommet center issue
    2) Add search input into Personality List and use new endpoint properly

--- packages/front/src/components/PersonalityList.jsx
+++ packages/front/src/components/PersonalityList.jsx
@@ -1,11 +1,63 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Card } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Card } from 'semantic-ui-react';
 
-class ClaimCreate extends Component {
+import { 
+  Box,
+  Grid,
+  Heading,
+  TextInput
+} from 'grommet';
+
+import * as Icon from 'grommet-icons';
+
+class PersonalityInputSearch extends Component {
     constructor(props) {
       super(props);
-      this.state = { personalities: [] };
+      this.timeout = 0;
+    }
+
+    doSearch(e) {
+      const searchText = e.target.value;
+      if (this.timeout) clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        this.props.callback(searchText);
+      }, 300);
+    }
+
+    render() {
+      return (
+        <Box
+          direction="row"
+          align="center"
+          pad={{ horizontal: "small", vertical: "xsmall" }}
+          round="small"
+          border={{
+            side: "all",
+            color: "border"
+          }}
+        >
+          <Icon.Search color="brand" />
+          <TextInput
+            type="search"
+            ref="searchInput"
+            type="text"
+            plain
+            placeholder="Type a name..."
+            onChange={e => this.doSearch(e)}
+          />
+        </Box>
+      )
+    }
+}
+
+class PersonalityList extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { 
+        personalities: [],
+        selectedOption: null
+      };
     }
 
     componentDidMount() {
@@ -13,31 +65,53 @@ class ClaimCreate extends Component {
         self.getPersonalities();
     }
 
-    getPersonalities() {
-      axios.get('http://localhost:3000/personality')
+    getPersonalities(name) {
+      const params = (name && { name }) || {};
+      console.log('params', params);
+      axios.get('http://localhost:3000/personality', {params})
         .then(response => {
-            const personalities = response.data;
+            const {personalities} = response.data;
             console.log(personalities);
             this.setState({ personalities });
         })
-        .catch(() => { console.log('Error while fetching personalities'); })
+        .catch((e) => { throw e })
     }
 
     render() {
-      const personalities = this.state.personalities;
+      const { personalities } = this.state;
+
       return (
-        <Card.Group>
-            {personalities.map((p, i) => (
-              <Card
-                  key={i}
-                  href={`personality/${p._id}`}
-                  header={p.name}
-                  description={p.bio}
-              />
-            ))}
-        </Card.Group>
+        <Grid
+          rows={['xxsmall', 'xxsmall', 'small']}
+          columns={['full']}
+          gap="small"
+          areas={[
+            { name: 'title', start: [0,0], end: [0,0]},
+            { name: 'search', start: [0,1], end: [0,1]},
+            { name: 'card', start:[0,2], end: [0,2] }
+          ]}
+        >
+          <Heading gridArea="title" level="2" margin="none">Choose a personality</Heading>
+          <Box gridArea="search">
+            <PersonalityInputSearch
+              callback={this.getPersonalities.bind(this)}
+            />
+          </Box>
+          <Box gridArea="card">
+            <Card.Group>
+                {personalities.map((p, i) => (
+                  <Card
+                      key={i}
+                      href={`personality/${p._id}`}
+                      header={p.name}
+                      description={p.bio}
+                  />
+                ))}
+            </Card.Group>
+          </Box>
+        </Grid>
       );
     }
 }
 
-export default ClaimCreate;
+export default PersonalityList;
