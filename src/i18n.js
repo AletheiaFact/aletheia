41f3d24f7db2566a0c64f734f52780d4fe38ab68
Mateus Santos <msantos@wikimedia.org>
2020-06-16 15:15:29 +0200

    Hotfix: change fallback language to en

--- src/i18n.js
+++ src/i18n.js
@@ -140,7 +140,7 @@ i18n.use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources,
         load: "languageOnly",
-        fallbackLng: "pt",
+        fallbackLng: "en",
         lng: navigator.language || navigator.userLanguage,
 
         keySeparator: false, // we do not use keys in form messages.welcome
