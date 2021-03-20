5993d36a5633a60b943d8e8ac56e993397e69905
mbsantos <msantos@wikimedia.org>
2021-03-20 19:42:16 +0100

    Create constant object for reviewed label colors. Apply it to ReviewStats

---
+++ src/constants/reviewColors.js
@@ -0,0 +1,11 @@
+export default {
+    "not-fact": "#111",
+    true: "#4dc24d",
+    "true-but": "#57e173",
+    arguable: "#d8d12f",
+    misleading: "#d35730",
+    false: "#d33730",
+    unsustainable: "#d7425e",
+    exaggerated: "#c1d259",
+    unverifiable: "#d89d2f"
+};
