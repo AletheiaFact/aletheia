53091d5d9549b08b284352f7f6ce11fcc9d995f3
mbsantos <msantos@wikimedia.org>
2021-03-20 19:45:32 +0100

    Finish UI according to vignette with i18n, proper api calls and current data present in the Claim resource.

--- src/components/Claim/ClaimCard.jsx
+++ src/components/Claim/ClaimCard.jsx
@@ -1,57 +1,137 @@
 import { Avatar, Button, Col, Comment, Row, Tooltip, Typography } from "antd";
-import React from "react";
+import React, { Component } from "react";
 import "./ClaimCard.less";
+import api from "../../api/claim";
+import { withTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import ReviewColors from "../../constants/reviewColors";
 
 const { Paragraph } = Typography;
-function ClaimCard(props) {
-    return (
-        <Col span={24}>
-            <Comment
-                style={{ margin: "0 20px" }}
-                key={props.claimIndex}
-                author={props.personality.name}
-                avatar={
-                    <Avatar
-                        src={props.personality.image}
-                        alt={props.personality.name}
-                    />
-                }
-                content={
-                    <>
-                        <Row className="claim-summary">
-                            <Col>
-                                <Paragraph
-                                    ellipsis={{ rows: 4, expandable: false }}
-                                >
-                                    "{props.claim.content || props.claim.title}"
-                                </Paragraph>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col offset={16}>
-                                <Button
-                                    shape="round"
-                                    type="primary"
-                                    onClick={e => {
-                                        e.stopPropagation();
-                                        props.viewClaim(props.claim._id);
-                                    }}
-                                >
-                                    Revisar
-                                </Button>
-                            </Col>
-                        </Row>
-                    </>
-                }
-                datetime={
-                    <Tooltip title="01/2020">
-                        <span>há 5 meses</span>
-                    </Tooltip>
-                }
-            />
-            <hr style={{ opacity: "20%" }} />
-        </Col>
-    );
+class ClaimCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            claim: this.props.claim
+        };
+    }
+    async componentDidMount() {
+        const claim = await api.getById(this.state.claim._id);
+        console.log(claim);
+        console.log(this.state.claim);
+        this.setState({
+            claim: Object.assign(claim, { content: this.state.claim.content })
+        });
+    }
+    render() {
+        const { t } = this.props;
+        return (
+            <Col span={24}>
+                <Comment
+                    style={{ margin: "0 20px" }}
+                    key={this.props.claimIndex}
+                    author={this.props.personality.name}
+                    avatar={
+                        <Avatar
+                            src={this.props.personality.image}
+                            alt={this.props.personality.name}
+                        />
+                    }
+                    content={
+                        <>
+                            <Row className="claim-summary">
+                                <Col>
+                                    <Paragraph
+                                        ellipsis={{
+                                            rows: 4,
+                                            expandable: false
+                                        }}
+                                    >
+                                        "
+                                        {this.state.claim.content ||
+                                            this.state.claim.title}
+                                        "
+                                    </Paragraph>
+                                    <Link
+                                        onClick={e => {
+                                            e.preventDefault();
+                                            return this.props.viewClaim(
+                                                this.state.claim._id
+                                            );
+                                        }}
+                                    >
+                                        {t("claim:cardLinkToFullText")}
+                                    </Link>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={16}>
+                                    <span
+                                        style={{
+                                            fontSize: "14px"
+                                        }}
+                                    >
+                                        {t("claim:metricsHeaderInfo", {
+                                            totalReviews: this.state.claim
+                                                ?.stats?.total
+                                        })}
+                                    </span>{" "}
+                                    <br />
+                                    <span
+                                        style={{
+                                            fontSize: "10px"
+                                        }}
+                                    >
+                                        {t("claim:cardOverallReviewPrefix")}{" "}
+                                        <span
+                                            style={{
+                                                color:
+                                                    ReviewColors[
+                                                        this.state.claim?.stats
+                                                            ?.reviews[0]._id
+                                                    ] || "#000",
+                                                fontWeight: "bold",
+                                                textTransform: "uppercase"
+                                            }}
+                                        >
+                                            {t(
+                                                `claimReviewForm:${this.state.claim?.stats?.reviews[0]._id}`
+                                            )}{" "}
+                                        </span>
+                                        (
+                                        {
+                                            this.state.claim?.stats?.reviews[0]
+                                                .count
+                                        }
+                                        )
+                                    </span>
+                                </Col>
+                                <Col span={8}>
+                                    <Button
+                                        shape="round"
+                                        type="primary"
+                                        onClick={e => {
+                                            e.stopPropagation();
+                                            this.props.viewClaim(
+                                                this.state.claim._id
+                                            );
+                                        }}
+                                    >
+                                        {t("claim:cardReviewButton")}
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </>
+                    }
+                    datetime={
+                        <Tooltip title={this.state.claim?.date}>
+                            <span>{this.state.claim?.date}</span>
+                        </Tooltip>
+                    }
+                />
+                <hr style={{ opacity: "20%" }} />
+            </Col>
+        );
+    }
 }
 
-export default ClaimCard;
+export default withTranslation()(ClaimCard);
