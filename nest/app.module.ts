225e89d007dff0c1e11e71280274acc7eff6e2aa
mbsantos <msantos@wikimedia.org>
2021-05-31 20:52:40 +0200

    Add Auth functionality using passport local strategy

--- nest/app.module.ts
+++ nest/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
 
 const mongodb_host = process.env.MONGODB_HOST || "localhost";
 const mongodb_name = process.env.MONGODB_NAME || "Aletheia";
@@ -14,6 +15,7 @@ const mongodb_name = process.env.MONGODB_NAME || "Aletheia";
             useFindAndModify: false,
         }),
         UsersModule,
+        AuthModule,
     ],
 })
 export class AppModule {}
--- nest/users/users.module.ts
+++ nest/users/users.module.ts
@@ -2,24 +2,26 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./schemas/user.schema";
 import { UsersService } from "./users.service";
+import { AuthService } from "../auth/auth.service";
 import { UsersController } from "./users.controller";
 
+const UserModel = MongooseModule.forFeatureAsync([
+    {
+        name: User.name,
+        useFactory: () => {
+            const schema = UserSchema;
+            schema.plugin(require("passport-local-mongoose"), {
+                usernameField: "email",
+            });
+            return schema;
+        },
+    },
+]);
+
 @Module({
-    imports: [
-        MongooseModule.forFeatureAsync([
-            {
-                name: User.name,
-                useFactory: () => {
-                    const schema = UserSchema;
-                    schema.plugin(require("passport-local-mongoose"), {
-                        usernameField: "email",
-                    });
-                    return schema;
-                },
-            },
-        ]),
-    ],
+    imports: [UserModel],
+    exports: [UsersService, UserModel],
     controllers: [UsersController],
-    providers: [UsersService],
+    providers: [UsersService, AuthService],
 })
 export class UsersModule {}
