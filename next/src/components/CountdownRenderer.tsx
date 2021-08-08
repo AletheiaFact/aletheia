8433ce7b3f23da964b56afa35db5cfe3427d56dd
mbsantos <msantos@wikimedia.org>
2021-08-08 17:55:47 +0200

    Introduce home page with countdown for publish/release

---
+++ next/src/components/CountdownRenderer.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from "styled-components";
+import colors from "../styles/colors";
+
+const TimeBoxDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h2 {
+        color: ${colors.lightBlue};
+        font-size: 5rem;
+        background: ${colors.bluePrimary};
+        padding: 1rem 2rem 0.5rem 2rem;
+        border-radius: 1rem;
+        text-align: center;
+    }
+
+    p {
+        font-size: 1.4rem;
+        text-transform: uppercase;
+        letter-spacing: 5px;
+    }
+`;
+
+const CountdownDiv = styled.div`
+    color: ${colors.lightBlue};
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 2.5rem;
+    @media (max-width: 380px) {
+        .hide {
+            display: none;
+        }
+
+        p {
+            font-size: 1rem;
+        }
+
+        h2 {
+            font-size: 4rem;
+        }
+    }
+`;
+
+const CountdownRenderer = ({ days, hours, minutes, seconds, completed }) => {
+    const hideDays = days > 0 ? "" : "hide";
+    const hideHours = hours > 0 ? "" : "hide";
+    const hideMinutes = days <= 0 && minutes > 0 ? "" : "hide";
+    const hideSeconds = hours <= 0 && seconds > 0 ? "" : "hide";
+
+    if (completed) {
+        // Render a completed state
+        return <span>Completo</span>;
+    } else {
+        // Render a countdown
+        return (
+            <CountdownDiv>
+                <TimeBoxDiv>
+                    <h2 id="days" className={hideDays}>
+                        {days}
+                        <p>Dias</p>
+                    </h2>
+                </TimeBoxDiv>
+                <TimeBoxDiv>
+                    <h2 id={hideHours}>
+                        {hours}
+                        <p>Horas</p>
+                    </h2>
+                </TimeBoxDiv>
+                <TimeBoxDiv className={hideMinutes}>
+                    <h2 id="minutes">
+                        {minutes}
+                        <p>Minutos</p>
+                    </h2>
+                </TimeBoxDiv>
+                <TimeBoxDiv className={hideSeconds}>
+                    <h2 id="seconds">
+                        {seconds}
+                        <p>Segundos</p>
+                    </h2>
+                </TimeBoxDiv>
+            </CountdownDiv>
+        );
+    }
+};
+
+export default CountdownRenderer;
--- next/src/pages/home.tsx
+++ next/src/pages/home.tsx
@@ -2,82 +2,11 @@ import React from "react";
 import { NextPage } from "next";
 import Countdown from "react-countdown";
 import { SocialIcon } from "react-social-icons";
-
-const colors = {
-    bluePrimary: "rgb(17, 39, 58)",
-    blueSecondary: "rgb(101, 126, 142)",
-    blueTertiary: "rgb(101, 126, 142)",
-    grayPrimary: "rgb(76, 77, 78)",
-    graySecondary: "rgb(151, 154, 155)",
-    grayTertiary: "rgb(194, 200, 204)",
-    lightBlue: "rgb(218, 232, 234)",
-};
-
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-    const secStyle = {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "space-evenly",
-    };
-
-    const secH2Style = {
-        color: colors.lightBlue,
-        fontSize: "8rem",
-        background: colors.bluePrimary,
-        padding: "2rem 3rem",
-        borderRadius: "1rem",
-    };
-
-    const secPStyle = {
-        fontSize: "1.4rem",
-        textTransform: "uppercase",
-        letterSpacing: "5px",
-    };
-
-    if (completed) {
-        // Render a completed state
-        return <span>Completo</span>;
-    } else {
-        // Render a countdown
-        return (
-            <div
-                className="count"
-                style={{
-                    color: colors.lightBlue,
-                    display: "flex",
-                    justifyContent: "center",
-                    gap: "2.5rem",
-                }}
-            >
-                <div className="days sec" style={secStyle}>
-                    <h2 id="days" style={secH2Style}>
-                        {days}
-                    </h2>
-                    <p style={secPStyle}>Days</p>
-                </div>
-                <div className="hours sec" style={secStyle}>
-                    <h2 id="hours" style={secH2Style}>
-                        {hours}
-                    </h2>
-                    <p style={secPStyle}>Hours</p>
-                </div>
-                <div className="minutes sec" style={secStyle}>
-                    <h2 id="minutes" style={secH2Style}>
-                        {minutes}
-                    </h2>
-                    <p style={secPStyle}>Minutes</p>
-                </div>
-                <div className="seconds sec" style={secStyle}>
-                    <h2 id="seconds" style={secH2Style}>
-                        {seconds}
-                    </h2>
-                    <p style={secPStyle}>Seconds</p>
-                </div>
-            </div>
-        );
-    }
-};
+import styled from "styled-components";
+import colors from "../styles/colors";
+import CountdownRenderer from "../components/CountdownRenderer";
+import Image from "next/image";
+import Logo from "../../public/images/logo1_white.svg";
 
 const Home: NextPage<{ data: string }> = (props) => {
     return (
@@ -99,37 +28,32 @@ const Home: NextPage<{ data: string }> = (props) => {
                         height: "80%",
                     }}
                 >
-                    <h1
-                        className="header"
+                    <div
                         style={{
-                            color: colors.lightBlue,
-                            textAlign: "center",
-                            fontSize: "2.6rem",
-                            letterSpacing: "3px",
-                            fontWeight: "500",
-                            wordSpacing: "4px",
-                            textTransform: "uppercase",
+                            position: "relative",
+                            width: "100%",
+                            height: "30vh",
                         }}
                     >
-                        AletheiaFact.org
-                    </h1>
+                        <Image src={Logo} layout="fill" />
+                    </div>
                     <h2
                         className="header"
                         style={{
                             color: colors.lightBlue,
                             textAlign: "center",
-                            fontSize: "2.6rem",
+                            fontSize: "1.8rem",
                             letterSpacing: "3px",
-                            fontWeight: "500",
+                            fontWeight: 500,
                             wordSpacing: "4px",
                             textTransform: "uppercase",
                         }}
                     >
-                        We're launching soon
+                        A contagem regressiva começou, estamos chegando!
                     </h2>
                     <Countdown
                         date={new Date(2021, 10, 1)}
-                        renderer={renderer}
+                        renderer={CountdownRenderer}
                     />
                 </div>
             </div>
@@ -137,25 +61,11 @@ const Home: NextPage<{ data: string }> = (props) => {
                 style={{
                     display: "flex",
                     justifyContent: "space-evenly",
+                    padding: "35px 25% 0 25%",
                 }}
             >
-                <a
-                    href="https://www.facebook.com/traveloperhabib"
-                    target="_blank"
-                    className="icon-link"
-                >
-                    <SocialIcon network="facebook" bgColor={colors.lightBlue} />
-                </a>
-                <a
-                    href="https://instagram.com/traveloperhabib"
-                    target="_blank"
-                    className="icon-link"
-                >
-                    <SocialIcon
-                        network="instagram"
-                        bgColor={colors.lightBlue}
-                    />
-                </a>
+                <SocialIcon network="facebook" bgColor={colors.lightBlue} />
+                <SocialIcon network="instagram" bgColor={colors.lightBlue} />
             </div>
         </main>
     );
