0361ec68651e8e2172f1ba465680513853745ca0
mbsantos <msantos@wikimedia.org>
2021-08-09 17:40:32 +0200

    Fix CSS flickering

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,30 @@
-import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-    render() {
-        const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps;
-        return (
-            <Html>
-                <Head />
-                <body style={{ margin: 0 }}>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
     }
 }
