06aa24194959808a2a5365046e292d5fb7811d84
mbsantos <msantos@wikimedia.org>
2021-09-20 20:59:52 -0300

    personality page fixes
    1) Fix nestjs view module import in personality module
    2) Fix relative paths in nextjs, use always absolute
    3) Fix claim list endpoint not querying with proper objectid
    field

--- src/components/Personality/PersonalityView.tsx
+++ src/components/Personality/PersonalityView.tsx
@@ -19,7 +19,7 @@ const PersonalityView = ({ personality, href }) => {
     const [showSpeechesSection, setShowSpeechesSection] = useState(true);
 
     const createClaim = () => {
-        const path = `./${personality._id}/claim/create`;
+        const path = `/personality/${personality._id}/claim/create`;
         router.push(path)
     }
 
