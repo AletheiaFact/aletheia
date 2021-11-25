cd4699465408d561d05735ab5cf43edcdaa6e52d
mbsantos <msantos@wikimedia.org>
2021-11-25 12:13:03 -0300

    Render different analytics scripts depending on production environment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { appWithTranslation } from 'next-i18next';
 import {Provider} from "react-redux";
 import { useStore } from "../store/store";
 import MainApp from "../components/MainApp";
+import * as umami from "../lib/umami";
 
 function MyApp({ Component, pageProps }) {
     const store = useStore();
@@ -17,7 +18,9 @@ function MyApp({ Component, pageProps }) {
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
-                <script async defer data-website-id="0c969da4-1502-4923-99e8-f70edd82957c" src="https://analytics.aletheiafact.org/umami.js"></script>
+                {umami.UMAMI_SITE_ID && (
+                    <script async defer data-website-id={umami.UMAMI_SITE_ID} src="https://analytics.aletheiafact.org/umami.js"></script>
+                )}
             </Head>
             <Provider store={store}>
                 <MainApp>
