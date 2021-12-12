07f9375285b1c81d00ebb88e600a557acb867e34
mbsantos <msantos@wikimedia.org>
2021-12-12 15:31:09 -0300

    Introduce Code of Conduct page and add it to the Cookie Consent popup

--- server/view/view.controller.ts
+++ server/view/view.controller.ts
@@ -50,6 +50,17 @@ export class ViewController {
             .render(req, res, "/privacy-policy-page", Object.assign(parsedUrl.query));
     }
 
+    @Get("code-of-conduct")
+    public async codeOfConductPage(@Req() req: Request, @Res() res: Response) {
+        const parsedUrl = parse(req.url, true);
+        // @ts-ignore
+        req.language = req.headers["accept-language"] || "en";
+
+        await this.viewService
+            .getNextServer()
+            .render(req, res, "/code-of-conduct-page", Object.assign(parsedUrl.query));
+    }
+
     @Get("_next*")
     public async assets(@Req() req: Request, @Res() res: Response) {
         const parsedUrl = parse(req.url, true);
