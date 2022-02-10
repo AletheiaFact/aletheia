8890be35d5c3091f032e15a6911be70d3a4f20df
Thais <thais.marpi@gmail.com>
2022-02-10 09:24:55 -0300

    Fix social media urls

--- src/components/AletheiaSocialMediaFooter.tsx
+++ src/components/AletheiaSocialMediaFooter.tsx
@@ -1,6 +1,6 @@
-import {SocialIcon} from "react-social-icons";
+import { SocialIcon } from "react-social-icons";
 import colors from "../styles/colors";
-import {Row} from "antd";
+import { Row } from "antd";
 import React from "react";
 
 const AletheiaSocialMediaFooter = () => {
@@ -14,8 +14,8 @@ const AletheiaSocialMediaFooter = () => {
         >
             <SocialIcon url="https://www.facebook.com/AletheiaFactorg-107521791638412" bgColor={colors.white} />
             <SocialIcon url="https://www.instagram.com/aletheiafact" bgColor={colors.white} />
-            <SocialIcon url="https://www.linkedin.com/in/aletheiafact-org" bgColor={colors.white} />
-            <SocialIcon url="https://www.github.com/in/aletheiafact" bgColor={colors.white} />
+            <SocialIcon url="https://www.linkedin.com/company/aletheiafact-org" bgColor={colors.white} />
+            <SocialIcon url="https://github.com/AletheiaFact/aletheia" bgColor={colors.white} />
         </Row>
     );
 }
