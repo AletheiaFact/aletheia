15637f7302b5d8a7721d3b2f2d48833804b7693d
Thais <thais.marpi@gmail.com>
2022-02-16 17:02:24 -0300

    Fix redirect after login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,19 @@
-import {NextPage} from "next";
-import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import { NextPage } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import LoginView from "../components/Login/LoginView";
 const parser = require('accept-language-parser');
 
 const LoginPage: NextPage<{ data: any }> = (props) => {
-    return <LoginView />
+    return <LoginView {...props} />
 }
 
-export async function getServerSideProps({ query, locale, locales, req }) {
+export async function getServerSideProps({ locale, locales, req }) {
     locale = parser.pick(locales, req.language) || locale || "en";
     return {
         props: {
             ...(await serverSideTranslations(locale)),
+            previousUrl: req.headers.referer || 'none',
+            host: req.protocol + '://' + req.get('host')
         },
     };
 }
