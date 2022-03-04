0f471aa5c03cbca27d51ab3901ab8f526f41b85d
pepermao <joaopedrobatistasantos501@gmail.com>
2022-03-03 22:19:27 -0300

    Add claimRevisionModule in personalityModule

--- server/personality/personality.module.ts
+++ server/personality/personality.module.ts
@@ -5,6 +5,7 @@ import { Personality, PersonalitySchema } from "./schemas/personality.schema";
 import { UtilService } from "../util";
 import { WikidataModule } from "../wikidata/wikidata.module";
 import { ClaimReviewModule } from "../claim-review/claim-review.module";
+import { ClaimRevisionModule } from "../claim-revision/claim-revision.module";
 import { PersonalityController } from "./personality.controller";
 import Logger from "../logger";
 import { ViewModule } from "../view/view.module";
@@ -17,7 +18,7 @@ const PersonalityModel = MongooseModule.forFeature([
 ]);
 
 @Module({
-    imports: [PersonalityModel, WikidataModule, ClaimReviewModule, ViewModule],
+    imports: [PersonalityModel, WikidataModule, ClaimReviewModule, ClaimRevisionModule, ViewModule],
     exports: [PersonalityService],
     providers: [UtilService, PersonalityService, Logger],
     controllers: [PersonalityController],
