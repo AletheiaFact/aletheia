082b623f13079dbb7e6f2432256240b0646998f8
pepermao <joaopedrobatistasantos501@gmail.com>
2022-03-24 22:03:40 -0300

    fixed yarn build

--- server/middleware/language.middleware.ts
+++ server/middleware/language.middleware.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 
 @Injectable()
 export class GetLanguageMiddleware implements NestMiddleware {
-    use(req: Request, res: Response, next: () => void) {
+    use(req: Request & { language: string }, next: () => void) {
         req.language = req.headers["accept-language"] || "en";
         next()
     }
