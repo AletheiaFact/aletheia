df1761689dddc4a4450d9539b6efdf374b4d5fe5
Karen Keiko <nascikeiko@gmail.com>
2022-04-05 18:26:35 -0300

    Introduce Cypress  and automated e2e tests#331
    Automated test e2e  in two code , the first the test make in route coming to the login page e the second the route coming to the home page

---
+++ cypress/support/locator.js
@@ -0,0 +1,16 @@
+const locators = {
+    LOGIN: {
+        USER: '#basic_email', 
+        PASSWORD: '#basic_password', 
+        BTN_LOGIN: '.ant-btn > span'
+
+    }, 
+
+    PERSONALITY:{
+        BTN_SEE_MORE_PERSONALITY:'[data-cy="testSeeMorePersonality"]',
+        BTN_ADD_PERSONALITY:'[data-cy=testButtonCreatePersonality]',
+        INPUT_SEARCH_PERSONALITY: '[data-cy="testInputSearchPersonality"]',
+    }
+    }
+
+export default locators;
