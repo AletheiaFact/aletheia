54a3cae85d5a43ce5d359a6e59a8a2c4faa310cd
Karen Keiko <nascikeiko@gmail.com>
2022-04-11 16:30:24 -0300

    Introduce Cypress and automated e2e tests #331
    Change to extension from .js to .ts

---
+++ cypress/integration/tests/functional.spec.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+import locators from "../../support/locator";
+
+describe("Should test at the login", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/login");
+
+        cy.title().should("be.equal", "AletheiaFact.org");
+        cy.get(locators.LOGIN.USER).type("test@aletheiafact.org", {
+            delay: 200,
+        });
+        cy.get(locators.LOGIN.PASSWORD).type("123456");
+        cy.get(locators.LOGIN.BTN_LOGIN).click();
+    });
+
+    it("Go personality and search new personality", () => {
+        cy.get(locators.PERSONALITY.BTN_SEE_MORE_PERSONALITY).click();
+        cy.get(locators.PERSONALITY.BTN_ADD_PERSONALITY).click();
+        cy.get(locators.PERSONALITY.INPUT_SEARCH_PERSONALITY).type("beyonce");
+        cy.get("[data-cy=testBackButton]").click();
+        cy.visit("http://localhost:3000/home");
+    });
+});
