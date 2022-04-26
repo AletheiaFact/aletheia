725f717be1a90a054257ad382b7b7dd727032163
Thais <thais.marpi@gmail.com>
2022-04-26 16:31:00 -0300

    Install Storybook
    Install Storybook using 'npx sb@next init' 
    Install the preset "@storybook/preset-ant-design"  to apply the Ant-design base layout

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "stories": [
+        "../src/stories/*.stories.*",
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/preset-ant-design",
+    ],
+    "framework": "@storybook/react",
+    "core": {
+        "builder": "@storybook/builder-webpack5"
+    },
+}
