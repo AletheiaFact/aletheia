9d5347d06c37270c7e2adf2264a5b6bb39e52f20
Thais <thais.marpi@gmail.com>
2022-06-13 12:03:49 -0300

    Update the seed script
    Don't try to create the user in Ory if there is an oryId on the seed user data

--- server/users/users.service.ts
+++ server/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Model } from "mongoose";
-import {Injectable, Logger} from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { User, UserDocument } from "./schemas/user.schema";
-import OryService from "../ory/ory.service";
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import OryService from '../ory/ory.service';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
@@ -13,25 +14,30 @@ export class UsersService {
     ) {}
 
     async findAll(getUsers): Promise<User[]> {
-        const {searchName} = getUsers;
+        const { searchName } = getUsers;
         return this.UserModel.aggregate([
-            { $match: {name: { '$regex': searchName, '$options': 'i' }}},
-            { $project: { "_id": 1, "name": 1}}
-        ])
+            { $match: { name: { $regex: searchName, $options: "i" } } },
+            { $project: { _id: 1, name: 1 } },
+        ]);
     }
 
     async register(user) {
-        const newUser = new this.UserModel(user)
-        const { data: oryUser } = await this.oryService.createIdentity(newUser, user.password);
-        newUser.oryId = oryUser.id;
-        try {
-            // @ts-ignore
-            return this.UserModel.register(
+        const newUser = new this.UserModel(user);
+
+        if (!newUser.oryId) {
+            const { data: oryUser } = await this.oryService.createIdentity(
                 newUser,
                 user.password
             );
+            newUser.oryId = oryUser.id;
+        }
+        try {
+            // @ts-ignore
+            return this.UserModel.register(newUser, user.password);
         } catch (e) {
-            await this.oryService.deleteIdentity(oryUser.id)
+            if (!newUser.oryId) {
+                await this.oryService.deleteIdentity(newUser.oryId);
+            }
         }
     }
 
