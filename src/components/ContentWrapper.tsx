6f59223ca56b166daac7a2a57ad7e963a1fa60be
thaismap <thais.marpi@gmail.com>
2022-07-01 11:04:20 -0300

    Remove personality page from desktop ready for now

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -25,7 +25,7 @@ const ContentWrapper = ({ children }) => {
     const router = useRouter();
 
     // TODO: we can remove this when we have desktop layout for all the pages
-    const desktopReadyPages = ["claim-review", "home", "personality-page"];
+    const desktopReadyPages = ["claim-review", "home"];
 
     const layout = desktopReadyPages.some((page) => router.pathname.includes(page))
         ? "desktop"
