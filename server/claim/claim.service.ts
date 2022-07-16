45c07664ebf14a06041d3e13e37fb2df036c6109
pepermao <joaopedrobatistasantos501@gmail.com>
2022-07-16 12:55:03 -0300

    add one more filter when we try to get specific revisions

--- server/claim/claim.service.ts
+++ server/claim/claim.service.ts
@@ -161,14 +161,20 @@ export class ClaimService {
     private async _getClaim(match: ClaimMatchParameters, revisionId = undefined, postprocess = true) {
         let claim
         if(revisionId) {
-            const rawClaim = await this.ClaimModel.aggregate([
-                { $match: match },
-                { $project: { "latestRevision": 0}}
-            ])
+            const rawClaim = await this.ClaimModel.findOne(match)
+                .select({"latestRevision": 0})
             // This line may cause a false positive in sonarCloud because if we remove the await, we cannot iterate through the results
-            const revision = (await this.claimRevisionService.getRevision(revisionId)).toObject()
+            const revision = await this.claimRevisionService.getRevision({
+                _id: revisionId,
+                personality: match.personality
+            })
+
+            if (!revision || !rawClaim) {
+                throw new NotFoundException()
+            }
+
             claim = {
-                ...rawClaim[0],
+                ...rawClaim,
                 revision
             }
         } else {
