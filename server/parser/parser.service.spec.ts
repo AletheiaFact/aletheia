714186333afa74626adc5ffd21227d8d2ac06647
msantos <msantos@wikimedia.org>
2022-07-18 12:46:28 -0300

    Fix remaining tests for ParserService

--- server/parser/parser.service.spec.ts
+++ server/parser/parser.service.spec.ts
@@ -39,14 +39,19 @@ describe('ParserService', () => {
             const claimText =
                 "Pellentesque auctor neque nec urna. Nulla facilisi. Praesent nec nisl a purus blandit viverra." +
                 "\n\nNam at tortor in tellus interdum sagittis. Ut leo. Praesent adipiscing. Curabitur nisi.";
-            const parseOutput = await parserService.parse(claimText)
+            const parseOutput = await (await parserService.parse(claimText))
+                    .populate({
+                    path: 'content',
+                    populate: {
+                        path: 'content',
+                    }
+                }).execPopulate();
 
             const paragraphs = parseOutput.content;
             expect(Array.isArray(paragraphs)).toBe(true);
             expect(paragraphs.length).toEqual(2);
-            // TODO: fix sentence tests
-            // expect(paragraphs[0].content.length).toEqual(3);
-            // expect(paragraphs[1].content.length).toEqual(4);
+            expect(paragraphs[0].content.length).toEqual(3);
+            expect(paragraphs[1].content.length).toEqual(4);
         });
 
         it("Music structure should not fail", async () => {
@@ -54,39 +59,60 @@ describe('ParserService', () => {
                 `${__dirname}/test-fixtures/claim_music.txt`,
                 "utf-8"
             );
-            const parseOutput = await parserService.parse(claimText);
+            const parseOutput = (await (await parserService.parse(claimText))
+                .populate({
+                    path: 'content',
+                    populate: {
+                        path: 'content',
+                    }
+                }).execPopulate()).toObject();
             const paragraphs = parseOutput.content;
-            const sentences = paragraphs[0].content;
             expect(paragraphs.length).toEqual(1);
-            // TODO: fix sentence tests
-            // expect(sentences.length).toEqual(46);
+            expect(paragraphs[0].content.length).toEqual(46);
         });
 
-        it.skip("parsed object conforms to schema", async () => {
+        it("parsed object conforms to schema", async () => {
             const claimText = "Nulla facilisi.\n\nUt leo.";
-            const parseOutput = await parserService.parse(claimText);
-            console.log(parseOutput)
-            // TODO: parser output schema tests
-            expect(Object.keys(parseOutput)).toContain(["object", "text"]);
+            const parseOutput = (await (await parserService.parse(claimText))
+                .populate({
+                    path: 'content',
+                    populate: {
+                        path: 'content',
+                    }
+                }).execPopulate()).toObject();
+            expect(Object.keys(parseOutput))
+                .toEqual(expect.arrayContaining(["content", "type"]))
+            expect(parseOutput.type).toEqual( "speech");
+
         });
 
         it("Ph.D word is not confused with end of sentence", async () => {
             const claimText = "Jose is Ph.D. and Maria is a Ph.D.";
-            const parseOutput = await parserService.parse(claimText);
+            const parseOutput = await (await parserService.parse(claimText))
+                .populate({
+                    path: 'content',
+                    populate: {
+                        path: 'content',
+                    }
+                }).execPopulate();
             const paragraphs = parseOutput.content;
             expect(paragraphs.length).toEqual( 1);
-            // TODO: fix sentence tests
-            // expect(paragraphs[0].content.length).toEqual( 1);
+            expect(paragraphs[0].content.length).toEqual(1);
         });
 
         it("Prefixes are not confused with end of sentence", async () => {
             const claimText =
                 "Mr. Jose and Mrs. Maria lives in St. Monica with Ms. Butterfly their Dr. of the year";
-            const parseOutput = await parserService.parse(claimText);
+            const parseOutput = await (await parserService.parse(claimText))
+                .populate({
+                    path: 'content',
+                    populate: {
+                        path: 'content',
+                    }
+                }).execPopulate();
             const paragraphs = parseOutput.content;
             expect(paragraphs.length).toEqual( 1);
-            // TODO: fix sentence tests
-            // expect(paragraphs[0].content.length).toEqual( 1);
+            expect(paragraphs[0].content.length).toEqual( 1);
         });
     });
 
