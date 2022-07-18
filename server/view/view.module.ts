099528238aad10dfaab5a9962babd9265c8df24f
msantos <msantos@wikimedia.org>
2022-07-18 11:19:30 -0300

    Add support for e2e and unit tests, starting with e2e tests for the personality creation endpoint

--- server/view/view.module.ts
+++ server/view/view.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ViewController } from "./view.controller";
 import { ViewService } from "./view.service";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-    imports: [],
+    imports: [ConfigModule],
     providers: [ViewService],
     controllers: [ViewController],
     exports: [ViewService]
