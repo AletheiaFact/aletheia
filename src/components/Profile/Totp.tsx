6a406d973a06f220fbbd4ff04c326f88de767477
Vitor Caneppele <vitor.a.caneppele@gmail.com>
2022-07-22 12:29:53 -0300

    Finished the TOTP functionality, making the user with TOTP enable pass to when Two steps of login

--- src/components/Profile/Totp.tsx
+++ src/components/Profile/Totp.tsx
@@ -1,28 +1,30 @@
-import {
-    SubmitSelfServiceSettingsFlowWithTotpMethodBody as ValuesType,
-} from "@ory/client";
-import { Alert, Form } from "antd";
-import { useTranslation } from "next-i18next";
+import { SubmitSelfServiceSettingsFlowWithTotpMethodBody as ValuesType } from "@ory/client";
+import { Form, Row, Typography } from "antd";
+import { Trans, useTranslation } from "next-i18next";
 import React, { useEffect, useState } from "react";
 import { orySubmitTotp } from "../../api/ory";
 import Button, { ButtonType } from "../Button";
 import InputPassword from "../InputPassword";
 import { getUiNode } from "../../lib/orysdk/utils";
 import { useRouter } from "next/router";
+import AletheiaButton from "../Button";
+import colors from "../../styles/colors";
 
 export const Totp = ({ flow, setFlow }) => {
     const [imgSource, setImgSource] = useState("");
+    const { Title } = Typography;
     const [textSource, setTextSource] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
     const [showForm, setShowForm] = useState(true);
     const { t } = useTranslation();
-    const router = useRouter()
+    const router = useRouter();
 
     let flowValues: ValuesType = {
         csrf_token: "",
         method: "totp",
         totp_code: "",
     };
+
     const getImageSource = () => {
         const img = getUiNode(flow, "id", "totp_qr");
         if (img) {
@@ -53,9 +55,9 @@ export const Totp = ({ flow, setFlow }) => {
             setShowForm(false);
         }
     }, [flow]);
-
+    //Remove When Task Finished
     useEffect(() => {
-        console.log(flow);
+        console.log(flow, "Flow");
     }, [flow]);
 
     const onSubmitLink = (values: ValuesType) => {
@@ -65,7 +67,7 @@ export const Totp = ({ flow, setFlow }) => {
             })
             .catch(() => {
                 setErrorMessage(
-                    "The provided authentication code is invalid, please try again."
+                    t("profile:totpIncorectCodeMessage")
                 );
             });
     };
@@ -97,42 +99,71 @@ export const Totp = ({ flow, setFlow }) => {
 
     return (
         <>
-            <h3>Manage 2FA TOTP Authenticator App</h3>
+            <Row>
+                <Typography.Title level={4}>
+                    {t("profile:totpSectionTittle")}
+                </Typography.Title>
+            </Row>
             {showForm && (
                 <Form onFinish={onFinish}>
-                    <p>
-                        Add a TOTP Authenticator App to your account to improve
-                        your account security. Popular Authenticator Apps are{" "}
-                        <a
-                            href="https://www.lastpass.com"
-                            rel="noreferrer"
-                            target="_blank"
+                    <Form.Item>
+                        <p>
+                            <Trans
+                                i18nKey={"profile:totpSectionDescription"}
+                                components={[
+                                    <a
+                                        style={{ whiteSpace: "pre-wrap" }}
+                                        href="https://www.lastpass.com"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    ></a>,
+                                    <a
+                                        style={{ whiteSpace: "pre-wrap" }}
+                                        href="https://apps.apple.com/us/app/google-authenticator/id388497605"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    ></a>,
+                                    <a
+                                        style={{ whiteSpace: "pre-wrap" }}
+                                        href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=en&gl=US"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    ></a>,
+                                ]}
+                            />
+                        </p>
+                        <img
+                            style={{
+                                display: "flex",
+                                marginLeft: "auto",
+                                marginRight: "auto",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                marginBottom: "20px",
+                            }}
+                            src={imgSource}
+                        />
+                        <p>{t("profile:totpCodeDescription")}</p>
+                        <code
+                            style={{
+                                borderRadius: "10px",
+                                backgroundColor: "#4a4c58",
+                                color: "#FFF",
+                                fontSize: "18px",
+                                display: "flex",
+                                padding: "10px",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
                         >
-                            LastPass
-                        </a>{" "}
-                        and Google Authenticator (
-                        <a
-                            href="https://apps.apple.com/us/app/google-authenticator/id388497605"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            iOS
-                        </a>
-                        ,{" "}
-                        <a
-                            href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=en&gl=US"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Android
-                        </a>
-                        ).
-                    </p>
-                    <img src={imgSource} />
-                    <Alert type="info" message={textSource} />
+                            {textSource}
+                        </code>
+                    </Form.Item>
                     <Form.Item
+                        validateStatus="error"
+                        help={errorMessage}
                         name="totp"
-                        label={t("Save")}
+                        label={t("profile:totpInputTittle")}
                         wrapperCol={{ sm: 24 }}
                         style={{
                             width: "100%",
@@ -140,19 +171,43 @@ export const Totp = ({ flow, setFlow }) => {
                         rules={[
                             {
                                 required: true,
-                                message: "Fill the field before submit",
+                                message: t("common:requiredFieldError"),
                             },
                         ]}
                     >
-                        <InputPassword />
+                        <InputPassword/>
                     </Form.Item>
                     <Button type={ButtonType.blue} htmlType="submit">
                         {t("login:submitButton")}
                     </Button>
                 </Form>
             )}
-            {!showForm && <Button onClick={onSubmitUnLink}>UnLink</Button>}
-            <p>{errorMessage}</p>
+            {!showForm && (
+                <AletheiaButton
+                    onClick={onSubmitUnLink}
+                    style={{
+                        width: "100%",
+                        marginTop: "21px",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        paddingBottom: 0,
+                    }}
+                    type={ButtonType.blue}
+                >
+                    <Title
+                        level={4}
+                        style={{
+                            fontSize: 14,
+                            color: colors.white,
+                            fontWeight: 400,
+                        }}
+                    >
+                        {t("profile:totpUnLinkSubmit")}
+                    </Title>
+                </AletheiaButton>
+            )}
         </>
+        //Find a better way to set this message
     );
 };
