d5c8c1819b62c22599d412d2c585d922810cac8f
thaismap <thais.marpi@gmail.com>
2022-08-04 20:56:52 -0300

    Add skeleton for sources list

--- src/components/Skeleton/SkeletonList.tsx
+++ src/components/Skeleton/SkeletonList.tsx
@@ -2,8 +2,6 @@ import { Row } from "antd";
 import React from "react";
 
 const SkeletonList = ({ listItem, repeat }) => {
-    const repetitions = Array(repeat);
-    console.log(repetitions);
     return (
         <Row gutter={40} style={{ marginTop: 32 }}>
             {[...Array(repeat)].map((_item, index) => (
