4586b04f596568868208226e9c6cd6fe29dc3d5c
thaismap <thais.marpi@gmail.com>
2022-08-15 18:14:11 -0300

    Add user tags

--- src/components/Kanban/KanbanCard.tsx
+++ src/components/Kanban/KanbanCard.tsx
@@ -1,16 +1,16 @@
-import { Col, Row, Typography } from "antd";
+import { Avatar, Col, Row, Typography } from "antd";
 import React from "react";
 
 import CardBase from "../CardBase";
+import UserTag from "./UserTag";
 
 const { Text, Paragraph } = Typography;
 
 const KanbanCard = ({ reviewTask }) => {
-    console.log(reviewTask);
     return (
         <a
             href={reviewTask.reviewHref}
-            style={{ width: "100%", minWidth: "320px" }}
+            style={{ width: "100%", minWidth: "330px" }}
         >
             <CardBase
                 style={{
@@ -37,30 +37,17 @@ const KanbanCard = ({ reviewTask }) => {
                             {reviewTask.sentenceContent}
                         </Paragraph>
                         <Text>{reviewTask.personalityName}</Text>
-                        <Text
-                            style={{
-                                fontSize: 12,
-                                width: "100%",
-                            }}
-                        >
+                    </Col>
+                    <Col
+                        span={24}
+                        style={{ display: "flex", justifyContent: "flex-end" }}
+                    >
+                        <Avatar.Group>
                             {reviewTask.usersName &&
                                 reviewTask.usersName.map((user, index) => {
-                                    return (
-                                        <>
-                                            <span style={{ marginRight: 5 }}>
-                                                {user}
-                                                {reviewTask.usersName.length -
-                                                    1 <=
-                                                index ? (
-                                                    <></>
-                                                ) : (
-                                                    ","
-                                                )}
-                                            </span>
-                                        </>
-                                    );
+                                    return <UserTag user={user} key={index} />;
                                 })}
-                        </Text>
+                        </Avatar.Group>
                     </Col>
                 </Row>
             </CardBase>
