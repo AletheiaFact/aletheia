98d10c8800c750bfbcbd974ab0aca2368e82e393
pepermao <joaopedrobatistasantos501@gmail.com>
2022-08-23 18:51:23 -0300

    enable switch to the old search

--- server/search/search.controller.ts
+++ server/search/search.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Logger, Query } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { ClaimRevisionService } from "../claim-revision/claim-revision.service";
 import { IsPublic } from "../decorators/is-public.decorator";
 import { PersonalityService } from "../personality/personality.service";
@@ -10,26 +11,34 @@ export class SearchController {
     constructor(
         private personalityService: PersonalityService,
         private sentenceService: SentenceService,
-        private claimRevisionService: ClaimRevisionService
+        private claimRevisionService: ClaimRevisionService,
+        private configService: ConfigService
     ) {}
 
     @IsPublic()
     @Get("api/search")
     async listAll(@Query() query) {
         const { pageSize, searchText, language } = query;
-
-        return Promise.all([
-            this.personalityService.findAll(searchText, pageSize, language),
-            this.sentenceService.findAll(searchText, pageSize),
-            this.claimRevisionService.findAll(searchText, pageSize),
-        ])
-            .then(([personalities, sentences, claims]) => {
-                return {
-                    personalities,
-                    sentences,
-                    claims,
-                };
-            })
-            .catch((error) => this.logger.error(error));
+        if (this.configService.get<string>("db.atlas")) {
+            return Promise.all([
+                this.personalityService.findAll(searchText, pageSize, language),
+                this.sentenceService.findAll(searchText, pageSize),
+                this.claimRevisionService.findAll(searchText, pageSize),
+            ])
+                .then(([personalities, sentences, claims]) => {
+                    return {
+                        personalities,
+                        sentences,
+                        claims,
+                    };
+                })
+                .catch((error) => this.logger.error(error));
+        } else {
+            return this.personalityService.combinedListAll({
+                name: searchText,
+                pageSize,
+                language,
+            });
+        }
     }
 }
