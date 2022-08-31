34470cb3131e4b88fade847b6d9ed146e89988ec
Karen Keiko <nascikeiko@gmail.com>
2022-08-31 19:01:50 -0300

    Update the classification input

--- src/components/Form/ClaimReviewSelect.tsx
+++ src/components/Form/ClaimReviewSelect.tsx
@@ -11,7 +11,7 @@ const { Option } = Select;
 const SelectInput = styled(Select)`
     background: ${colors.white};
     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-    border-radius: 30px;
+    border-radius: 4px;
     border: none;
     height: 40px;
     width: 100%;
