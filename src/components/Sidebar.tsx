d5041471ac021f4b58674076276dfe227d573403
thaismap <thais.marpi@gmail.com>
2022-09-12 09:32:18 -0300

    Use drawer in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,57 +1,27 @@
-import { CloseOutlined } from "@ant-design/icons";
-import { Button, Col, Layout, Row } from "antd";
+import { Drawer } from "antd";
 import React from "react";
 
 import colors from "../styles/colors";
 import AletheiaMenu from "./AletheiaMenu";
 import Logo from "./Header/Logo";
 
-const { Sider } = Layout;
 const Sidebar = ({ menuCollapsed, onToggleSidebar }) => {
     return (
-        <Sider
-            collapsible
-            defaultCollapsed={menuCollapsed}
-            collapsed={menuCollapsed}
-            collapsedWidth={0}
-            width={270}
-            style={{
-                position: "fixed",
-                top: 0,
-                left: 0,
-                height: "100vh",
-                overflow: "hidden",
-                zIndex: 4,
-                background: "#F5F5F5",
-                boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.25)",
+        <Drawer
+            visible={!menuCollapsed}
+            onClose={onToggleSidebar}
+            width="270px"
+            placement="left"
+            bodyStyle={{ padding: 0 }}
+            drawerStyle={{
+                backgroundColor: colors.lightGray,
+                alignItems: "flex-start",
             }}
+            closable={false}
         >
-            <Row
-                style={{
-                    padding: "15px 20px",
-                }}
-            >
-                <Col span={20}>
-                    <Logo color={colors.bluePrimary} />
-                </Col>
-                <Col span={4}>
-                    <Button
-                        data-cy="testCloseSideMenu"
-                        onClick={() => onToggleSidebar()}
-                        type="text"
-                    >
-                        <CloseOutlined
-                            style={{
-                                fontSize: "22px",
-                                color: colors.bluePrimary,
-                                marginTop: "5px",
-                            }}
-                        />
-                    </Button>
-                </Col>
-            </Row>
+            <Logo color={colors.bluePrimary} />
             <AletheiaMenu />
-        </Sider>
+        </Drawer>
     );
 };
 
