cd396ed144b987d9be8b5434c2500b8e51d6c72c
thaismap <thais.marpi@gmail.com>
2022-09-12 14:03:22 -0300

    Remove unused imports

--- src/components/AletheiaMenu.tsx
+++ src/components/AletheiaMenu.tsx
@@ -1,7 +1,7 @@
 import { Menu } from "antd";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 
 import { useAppSelector } from "../store/store";
--- src/components/Search/SearchOverlay.tsx
+++ src/components/Search/SearchOverlay.tsx
@@ -1,4 +1,4 @@
-import { LeftCircleFilled, SearchOutlined } from "@ant-design/icons";
+import { LeftCircleFilled } from "@ant-design/icons";
 import { Col } from "antd";
 import React from "react";
 import { useDispatch } from "react-redux";
