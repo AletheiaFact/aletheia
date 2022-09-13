714bdfae53ab23300703529cf8c5876ec4f79df8
msantos <msantos@wikimedia.org>
2022-09-13 10:48:07 +0200

    Fix 403 error with /api/health endpoint. This is crashing the pod restart

--- server/auth/session.guard.ts
+++ server/auth/session.guard.ts
@@ -52,9 +52,12 @@ export class SessionGuard implements CanActivate {
     }
 
     next(request, response, isPublic) {
-        const isAllowedPublicUrl = ["/login", "/_next", "/api/.ory"].some(
-            (route) => request.url.startsWith(route)
-        );
+        const isAllowedPublicUrl = [
+            "/login",
+            "/_next",
+            "/api/.ory",
+            "/api/health",
+        ].some((route) => request.url.startsWith(route));
         const overridePublicRoutes =
             !isAllowedPublicUrl &&
             this.configService.get<string>("override_public_routes");
