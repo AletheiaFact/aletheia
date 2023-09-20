34cf6ffd1b0d342e8fcb45f090026d654bc34262
msantos <msantos@wikimedia.org>
2023-09-20 19:11:54 +0200

    Get NOVU_API_KEY from the config yaml

--- server/notifications/novu.provider.ts
+++ server/notifications/novu.provider.ts
@@ -7,9 +7,8 @@ const NOVU_PROVIDER_TOKEN = "NOVU_PROVIDER_TOKEN";
 export const NovuProvider: Provider = {
     provide: NOVU_PROVIDER_TOKEN,
     inject: [ConfigService],
-    useFactory: () => {
-        const NOVU_API_KEY = process.env.NOVU_API_KEY;
-        return new Novu(NOVU_API_KEY);
+    useFactory: (configService: ConfigService) => {
+        return new Novu(configService.get<string>("novu.api_key"));
     },
 };
 
