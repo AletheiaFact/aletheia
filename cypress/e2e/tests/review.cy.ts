265bed15584b4261bfa8a055fed150f8c51c5fd6
pepermao <joaopedrobatistasantos501@gmail.com>
2023-11-12 19:28:34 +0100

    remove click error

--- cypress/e2e/tests/review.cy.ts
+++ cypress/e2e/tests/review.cy.ts
@@ -30,9 +30,7 @@ describe("Test claim review", () => {
         );
         cy.get("[data-cy=frase1]").click();
         cy.get(locators.claim.BTN_SEE_FULL_REVIEW).should("exist");
-        cy.get(locators.claimReview.BTN_START_CLAIM_REVIEW)
-            .should("not.exist")
-            .click();
+        cy.get(locators.claimReview.BTN_START_CLAIM_REVIEW).should("not.exist");
     });
 
     it("should be able to assign a user", () => {
