d568ce97d892c96a65d0ca781a21d5ba3db630e0
msantos <msantos@wikimedia.org>
2023-11-29 19:07:01 +0100

    Hotfix: the env variable needs to have the NEXT_PUBLIC prefix in order to work

--- src/lib/orysdk/index.ts
+++ src/lib/orysdk/index.ts
@@ -8,7 +8,7 @@ import { edgeConfig } from "@ory/integrations/next";
 
 let oryConfig = new Configuration(edgeConfig);
 
-if (process.env.ENVIRONMENT === "production") {
+if (process.env.NEXT_PUBLIC_ENVIRONMENT === "production") {
     oryConfig = new Configuration({
         basePath: process.env.NEXT_PUBLIC_ORY_SDK_URL,
         baseOptions: {
