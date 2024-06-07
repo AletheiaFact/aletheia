bf819d20e61ff1b7a62a2c9887d143b0fcdac550
pepermao <joaopedrobatistasantos501@gmail.com>
2024-06-07 23:28:50 +0200

    Add claim list page title

--- src/components/Claim/ClaimListView.tsx
+++ src/components/Claim/ClaimListView.tsx
@@ -2,11 +2,18 @@ import { Row, Col } from "antd";
 import React from "react";
 import ClaimList from "./ClaimList";
 import SourceList from "../Source/SourceList";
+import { useTranslation } from "react-i18next";
 
 const ClaimListView = () => {
+    const { t } = useTranslation();
     return (
         <>
             <Row justify="center" style={{ marginTop: "64px" }}>
+                <Col sm={22} md={14} lg={21}>
+                    <h1 style={{ fontSize: 32 }}>
+                        {t("claim:claimListTitle")}
+                    </h1>
+                </Col>
                 <Col sm={22} md={14} lg={12}>
                     <SourceList />
                 </Col>
