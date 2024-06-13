18cdffdf496239e09da747508a500f2e75b87fc9
caneppelevitor <vitor.a.caneppele@gmail.com>
2024-06-13 21:31:51 +0200

    Added FIXME comment on db config file

--- server/config/db.config.ts
+++ server/config/db.config.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from "@nestjs/config";
 
+// FIXME: Find a better way to inject configService directly into modules
 export default {
     type: new ConfigService().get<string>("db.type") || "mongodb",
 };
