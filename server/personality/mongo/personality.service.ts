1eae0fbc1410aaf7126cff90ce6d370997d43005
caneppelevitor <vitor.a.caneppele@gmail.com>
2024-06-13 19:46:57 +0200

    Removed unused interface import and properly returning error on catch

--- server/personality/mongo/personality.service.ts
+++ server/personality/mongo/personality.service.ts
@@ -22,7 +22,6 @@ import {
     ICombinedListResult,
     IFindAllOptions,
     IFindAllResult,
-    IPersonality,
 } from "../../interfaces/personality.interface";
 
 @Injectable({ scope: Scope.REQUEST })
@@ -444,7 +443,7 @@ export class MongoPersonalityService {
             })
             .catch((error) => {
                 this.logger.error(error);
-                return Promise.reject(error);
+                return error;
             });
     }
 
