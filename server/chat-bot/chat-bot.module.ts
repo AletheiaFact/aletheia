7bed923908dcf195eeeed4103ec8b1d65e966da7
caneppelevitor <vitor.a.caneppele@gmail.com>
2024-06-26 11:46:52 -0300

    Created middleware to authenticate webHook api requests

--- server/chat-bot/chat-bot.module.ts
+++ server/chat-bot/chat-bot.module.ts
@@ -1,13 +1,26 @@
-import { Module } from "@nestjs/common";
+import {
+    MiddlewareConsumer,
+    Module,
+    NestModule,
+    RequestMethod,
+} from "@nestjs/common";
 import { ChatbotService } from "./chat-bot.service";
 import { ChatbotController } from "./chat-bot.controller";
 import { HttpModule } from "@nestjs/axios";
 import { VerificationRequestModule } from "../verification-request/verification-request.module";
 import { ConfigModule } from "@nestjs/config";
+import { AuthZenviaWebHookMiddleware } from "../middleware/auth-zenvia-webhook.middleware";
 
 @Module({
     imports: [HttpModule, VerificationRequestModule, ConfigModule],
     providers: [ChatbotService],
     controllers: [ChatbotController],
 })
-export class ChatbotModule {}
+export class ChatbotModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(AuthZenviaWebHookMiddleware).forRoutes({
+            path: "api/chatbot/hook",
+            method: RequestMethod.POST,
+        });
+    }
+}
