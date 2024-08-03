83995a5b73948215265a18f7ffaad47e40820278
caneppelevitor <vitor.a.caneppele@gmail.com>
2024-08-02 22:51:12 -0300

    Added missing text

--- server/chat-bot/chat-bot-actions.ts
+++ server/chat-bot/chat-bot-actions.ts
@@ -33,7 +33,7 @@ const MESSAGES = {
     noTextMessageNoMessage:
         "Desculpe, só podemos processar mensagens de texto. Por favor, envie sua mensagem em formato de texto.\n\nNosso trabalho é verificar informações falsas.\n\nSe quiser saber mais sobre o que fazemos, visite: https://aletheiafact.org. Se mudar de ideia e desejar fazer uma denúncia, basta digitar DENÚNCIA a qualquer momento.",
     notUnderstood:
-        "Desculpe, não entendi sua resposta. Para continuar, preciso que você digite SIM se deseja fazer uma denúncia, ou NÃO se não deseja.\n\nVocê gostaria de fazer uma denúncia agora?",
+        "Desculpe, não entendi sua resposta. Para continuar, preciso que você digite SIM se deseja fazer uma denúncia, ou NÃO se prefere falar com uma pessoa real.\n\nVocê gostaria de fazer uma denúncia agora?",
     askForVerificationRequest:
         "Por favor, me conte com detalhes o que você gostaria de denunciar.\n\nPor favor, inclua todas as informações que considerar relevantes para que possamos verificar a denúncia de forma eficiente 👀",
     noTextMessageAskForVerificationRequest:
