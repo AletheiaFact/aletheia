69ef022eba0de6e176198cdfa6d1b676fc23138d
msantos <msantos@wikimedia.org>
2024-08-13 22:22:43 +0200

    Fix cypress racing condition when adding a question to the fact-checking report

--- cypress/e2e/tests/review.cy.ts
+++ cypress/e2e/tests/review.cy.ts
@@ -76,6 +76,7 @@ describe("Test claim review", () => {
             });
         });
         goToClaimReviewPage();
+        cy.wait(4000);
         cy.get(locators.claimReview.INPUT_CLASSIFICATION)
             .should("exist")
             .click();
@@ -83,6 +84,7 @@ describe("Test claim review", () => {
             .should("be.visible")
             .click();
         cy.get(locators.claimReview.BTN_ADD_QUESTION).should("exist").click();
+        cy.wait(2000);
         cy.get(locators.claimReview.INPUT_QUESTION)
             .eq(1)
             .type(review.question2);
