9c37241fac913725da36c9d4f6648f830938111f
pepermao <joaopedrobatistasantos501@gmail.com>
2024-08-16 14:48:21 +0200

    Try fix pipeline build

--- next.config.js
+++ next.config.js
@@ -9,9 +9,21 @@ module.exports = withAntdLess({
     distDir: "./dist/.next",
     i18n,
     webpack(config) {
+        config.module.rules.push({
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: "javascript/auto",
+        });
+
         return config;
     },
     images: {
         domains: ["i.creativecommons.org", "upload.wikimedia.org"],
+    },
+    compiler: {
+        styledComponents: {
+            displayName: true,
+            ssr: true,
+        },
     },
 });
