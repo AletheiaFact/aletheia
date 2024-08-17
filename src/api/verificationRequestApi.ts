1d09fa51573fa6cc1dfefd1439b699d2943b67a5
caneppelevitor <vitor.a.caneppele@gmail.com>
2024-08-17 15:44:17 +0200

    Applied Verification Requests's Query Pagination

--- src/api/verificationRequestApi.ts
+++ src/api/verificationRequestApi.ts
@@ -63,6 +63,7 @@ const get = (options: SearchOptions, dispatch = null) => {
                     data: verificationRequests,
                     total: totalVerificationRequests,
                     totalPages,
+                    totalVerificationRequests,
                 };
             }
 
--- src/components/VerificationRequest/VerificationRequestList.tsx
+++ src/components/VerificationRequest/VerificationRequestList.tsx
@@ -6,39 +6,44 @@ import verificationRequestApi from "../../api/verificationRequestApi";
 import AletheiaButton from "../Button";
 import { Grid } from "@mui/material";
 import {
+    DataGrid,
     GridActionsCellItem,
     GridColDef,
     GridRowParams,
 } from "@mui/x-data-grid";
 import colors from "../../styles/colors";
-import PaginatedDataGrid from "../PaginetedDataGrid";
 
 const VerificationRequestList = () => {
     const { t } = useTranslation();
     const [nameSpace] = useAtom(currentNameSpace);
     const [verificationRequests, setVerificationRequests] = useState([]);
-
-    const truncateWithEllipsis = (value, maxLength) => {
-        if (!value) return "";
-        const truncatedValue = value.substring(0, maxLength);
-        return value.length > maxLength
-            ? `${truncatedValue}...`
-            : truncatedValue;
-    };
+    const [totalVerificationRequests, setTotalVerificationRequests] =
+        useState(0);
+    const [paginationModel, setPaginationModel] = React.useState({
+        pageSize: 10,
+        page: 0,
+    });
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await verificationRequestApi.get({});
-                if (response && response.data) {
+                const response = await verificationRequestApi.get({
+                    page: paginationModel.page + 1,
+                    pageSize: paginationModel.pageSize,
+                });
+                if (response) {
                     setVerificationRequests(response.data);
+                    setTotalVerificationRequests(
+                        response.totalVerificationRequests
+                    );
                 }
             } catch (error) {
                 console.error("Error fetching verification requests:", error);
             }
         };
+
         fetchData();
-    }, [nameSpace]);
+    }, [paginationModel, nameSpace]);
 
     const handleRedirect = useCallback(
         (data_hash) => () => {
@@ -126,6 +131,14 @@ const VerificationRequestList = () => {
         [handleRedirect, t]
     );
 
+    const truncateWithEllipsis = useCallback((value, maxLength) => {
+        if (!value) return "";
+        const truncatedValue = value.substring(0, maxLength);
+        return value.length > maxLength
+            ? `${truncatedValue}...`
+            : truncatedValue;
+    }, []);
+
     return (
         <Grid
             container
@@ -140,9 +153,16 @@ const VerificationRequestList = () => {
                 </h2>
             </Grid>
             <Grid item xs={10} sx={{ height: "auto", overflow: "auto" }}>
-                <PaginatedDataGrid
+                <DataGrid
                     rows={verificationRequests}
                     columns={columns}
+                    paginationModel={paginationModel}
+                    pageSizeOptions={[5, 10, 50]}
+                    onPaginationModelChange={setPaginationModel}
+                    getRowId={(row) => row._id}
+                    autoHeight
+                    rowCount={totalVerificationRequests}
+                    paginationMode="server"
                     sx={{
                         "& .MuiDataGrid-columnHeader": {
                             backgroundColor: colors.lightGraySecondary,
