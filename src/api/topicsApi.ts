0408fa82b2cb9a525e5873ae995ad5105b9e6714
caneppelevitor <vitor.a.caneppele@gmail.com>
2024-08-27 23:27:12 +0200

    Enabled topics search and Application of topic and content filters on verificationRequest

--- src/api/topicsApi.ts
+++ src/api/topicsApi.ts
@@ -13,6 +13,24 @@ const request = axios.create({
     baseURL: `/api/topics`,
 });
 
+const searchTopics = ({ query, dispatch }) => {
+    const params = { query };
+    return request
+        .get(`/search`, { params })
+        .then((response) => {
+            const topicResults = response.data;
+            dispatch({
+                type: ActionTypes.RESULTS_TOPICS_AUTOCOMPLETE,
+                results: topicResults,
+            });
+            return topicResults;
+        })
+        .catch((e) => {
+            message.error("Failed to fetch topics");
+            return [];
+        });
+};
+
 const getTopics = ({ topicName, t, dispatch }: IGetTopicsOptions) => {
     const params = {
         topicName,
@@ -52,6 +70,7 @@ const createTopics = (params, t) => {
 const TopicsApi = {
     createTopics,
     getTopics,
+    searchTopics,
 };
 
 export default TopicsApi;
