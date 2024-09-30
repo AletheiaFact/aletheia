4d07bba04600ba9fb1f6c6d40a52d59a04da0454
LuizFNJ <luizfnj3103@gmail.com>
2024-09-30 20:36:18 +0200

    alumialab configuration

--- config/localConfig.example.ts
+++ config/localConfig.example.ts
@@ -1,18 +1,27 @@
 import { LocalConfig } from "./localConfig.schema";
 
 const localConfig: LocalConfig = {
-    theme: {},
+    theme: {
+        colors: {
+            bluePrimary: "#EC5F2A",
+            white: "#f5f5f5",
+            blackTertiary: "#1b1b1b",
+            lightBlueSecondary: "#EC5F2A",
+            lightBlueMain: "#f5f5f5"
+        }
+    },
     footer: {
-        socialMedias: [],
+        socialMedias: ["teste 1", "teste 2", "teste 3", "teste 4"],
         showStatuteButton: {
             show: true,
-            redirectUrl: "",
+            redirectUrl: "testandolinksStatuteButton",
         },
+        repositoryLink: " https://github.com/AletheiaFact/Aletheia",
     },
     header: {
         donateButton: {
             show: true,
-            redirectUrl: "",
+            redirectUrl: "TestandoDonateButton",
         },
     },
     home: {
