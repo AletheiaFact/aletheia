084e7b2622283eed6a8bd7198a504ef16dde2cf5
LuizFNJ <luizfnj3103@gmail.com>
2024-10-02 03:30:00 +0200

    using nodeJS for the logo svg

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -4,20 +4,23 @@ import colors from "../../styles/colors";
 import { useAtom } from "jotai";
 import { currentNameSpace } from "../../atoms/namespace";
 import { NameSpaceEnum } from "../../types/Namespace";
+import Image from "next/image";
 
 const Logo = ({
     color = colors.logoWhite,
     height = "42px",
+    width = "80px",
     lineHeight = "24px",
 }) => {
     const [nameSpace] = useAtom<string>(currentNameSpace);
     const logoSvgPath = process.env.LOGO_SVG_PATH || '/images/logo1_white.svg';
     if (nameSpace === NameSpaceEnum.Main) {
         return (
-            <img
+            <Image
                 alt="logo"
                 src={logoSvgPath}
                 height={height}
+                width={width}
             />
         );
     } else if (nameSpace) {
