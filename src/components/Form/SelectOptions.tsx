b2cc9441d68df1288a8208e6660aec55040a1f45
LuizFNJ <luizfnj3103@gmail.com>
2025-02-28 20:52:29 +0100

    Migrate select antd

--- src/components/Form/SelectOptions.tsx
+++ src/components/Form/SelectOptions.tsx
@@ -1,4 +1,5 @@
-import { Select } from "antd";
+import { Autocomplete, TextField } from "@mui/material";
+
 import { useTranslation } from "next-i18next";
 import React, { useMemo, useState } from "react";
 import styled from "styled-components";
@@ -9,12 +10,12 @@ import { useAtom } from "jotai";
 import { currentNameSpace } from "../../atoms/namespace";
 import { currentUserRole } from "../../atoms/currentUser";
 
-const StyledSelect = styled(Select)`
-    .ant-select-selector {
-        border-radius: 4px;
+const StyledSelect = styled(Autocomplete)`
+    .MuiOutlinedInput-root {
+        border-radius: 0px;
     }
-
-    .ant-select-selection-placeholder {
+    
+    .MuiInputLabel-root {
         color: ${colors.neutralSecondary};
     }
 `;
@@ -28,10 +29,12 @@ function SelectOptions({
     preloadedOptions = [],
     preloadedTopics = [],
     loading = false,
+    onChange,
     ...props
 }) {
     const [fetching, setFetching] = useState(false);
     const [options, setOptions] = useState(preloadedOptions);
+    const [selectedValue, setSelectedValue] = useState(value || []);
 
     const filteredOptions = options.filter(({ value }) => {
         const slug = value.toLowerCase().replace(" ", "-");
@@ -68,16 +71,36 @@ function SelectOptions({
     }, [fetchOptions, fieldName, nameSpace, t]);
 
     return (
-        <StyledSelect
-            mode={mode}
-            showSearch
-            filterOption={false}
-            onSearch={getOptions}
-            notFoundContent={fetching ? <Loading /> : null}
+        <StyledSelect multiple freeSolo
             options={filteredOptions}
-            tokenSeparators={[","]}
+            getOptionLabel={(option) => option ? option.label || option : ""}
+            isOptionEqualToValue={(option, value) => option?.value === value?.value}
+            loading={fetching ? <Loading /> : selectedValue}
+            value={value || []}
+            onChange={(_event, newValue) => {
+                if (Array.isArray(newValue)) {
+                    setSelectedValue(newValue);
+                    const selectedIds = newValue.map(item => item?.value);
+                    onChange(selectedIds);
+                }
+            }}
+            onInputChange={getOptions}
             style={{ ...style }}
-            value={value}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label={props.placeholder}
+                    variant="outlined"
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <>
+                                {params.InputProps.endAdornment}
+                            </>
+                        ),
+                    }}
+                />
+            )}
             {...props}
         />
     );
