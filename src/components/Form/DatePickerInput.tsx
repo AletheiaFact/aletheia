6b750aa68360e694a67047ccafc9025e196da670
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-04-14 09:49:58 -0300

    migrate DatePicker from antd to MUI-X

--- src/components/Form/DatePickerInput.tsx
+++ src/components/Form/DatePickerInput.tsx
@@ -1,31 +1,73 @@
-import { DatePicker } from "antd";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TextField } from "@mui/material";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import colors from "../../styles/colors";
 import styled from "styled-components";
+import { useTranslation } from "next-i18next";
+import { useState } from "react";
+import dayjs from "dayjs";
 
-const DatePickerInput = styled(DatePicker)`
-    background: ${(props) => (props.white ? colors.white : colors.lightNeutral)};
-    box-shadow: 0px 2px 2px ${colors.shadow};
-    border-radius: 4px;
-    border: none;
-    height: 40px;
-    width: 100%;
+const StyledTextField = styled(TextField)`
+  background: ${(props) => (props.$white ? colors.white : colors.lightNeutral)};
+  box-shadow: 0px 2px 2px ${colors.shadow};
+  border-radius: 4px;
+  border: none;
+  height: 48px;
+  width: 100%;
 
-    input::placeholder {
-        color: ${colors.blackSecondary};
+  input {
+    font-size: 16px;
+  }
+
+  input::placeholder {
+    color: ${colors.blackSecondary};
+  }
+
+  .MuiOutlinedInput-root {
+    fieldset {
+      border: none;
     }
 
-    :focus {
-        border: none;
-        box-shadow: 0px 2px 2px ${colors.shadow};
+    &:hover fieldset {
+      border: none;
     }
 
-    :active {
-        border: none;
+    &.Mui-focused fieldset {
+      border: none;
     }
-
-    :hover {
-        border: none;
-    }
+  }
 `;
+
+const DatePickerInput = (props) => {
+  const { t } = useTranslation();
+  const [value, setValue] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        label={t(props.placeholder)}
+        value={value}
+        inputFormat="DD/MM/YYYY"
+        open={open}
+        onClose={() => setOpen(false)}
+        onChange={(newValue) => {
+          setValue(newValue);
+          props.onChange?.(newValue);
+        }}
+        maxDate={dayjs()}
+        renderInput={(params) =>
+          <StyledTextField
+            {...params}
+            onClick={() => setOpen(true)}
+            {...props}
+          />}
+        PopperProps={{ placement: 'bottom-start', }}
+        desktopModeMediaQuery="@media (min-width: 0)"
+      />
+    </LocalizationProvider>
+  );
+};
 
 export default DatePickerInput;
