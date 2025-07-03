82ed5ea2003ea497c480c64c364678dda3f5f5a9
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-07-03 11:51:30 -0300

    logout user if not found in MongoDB after Ory login

--- server/auth/session.guard.ts
+++ server/auth/session.guard.ts
@@ -3,10 +3,21 @@ import { Configuration, FrontendApi } from "@ory/client";
 import { Roles } from "./ability/ability.factory";
 import { Logger } from "@nestjs/common";
 import { BaseGuard } from "./base.guard";
+import { Reflector } from "@nestjs/core";
+import { UsersService } from "users/users.service";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class SessionGuard extends BaseGuard {
     private readonly logger = new Logger(SessionGuard.name);
+
+    constructor(
+        private readonly configService: ConfigService,
+        reflector: Reflector,
+        private readonly UsersService: UsersService
+    ) {
+        super(configService, reflector);
+    }
 
     // @ts-ignore
     async canActivate(
@@ -68,6 +79,34 @@ export class SessionGuard extends BaseGuard {
                     role: session?.identity?.traits?.role,
                     status: session?.identity.state,
                 };
+
+                const mongoUserId = session?.identity?.traits?.user_id;
+                const user = await this.UsersService.getById(mongoUserId);
+
+                if (!user) {
+                    this.logger.error(`User not found for ID: ${mongoUserId}`);
+                    try {
+                        const { data } = await ory.createBrowserLogoutFlow({
+                            cookie: request.header("Cookie"),
+                        });
+                        await ory.updateLogoutFlow({
+                            cookie: request.header("Cookie"),
+                            token: data.logout_token,
+                        });
+                    } catch (error) {
+                        this.logger.error(
+                            "Error during logout flow for missing DB user",
+                            error
+                        );
+                    }
+                    return this.checkAndRedirect(
+                        request,
+                        response,
+                        isPublic,
+                        "/unauthorized"
+                    );
+                }
+
                 const overridePublicRoutes =
                     session?.identity?.traits?.role.main === Roles.Regular &&
                     this.configService.get<string>("override_public_routes");
