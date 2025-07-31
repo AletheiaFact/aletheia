613d379c39191f9dd8b2fa4d22f0ebd7e5639169
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-07-31 13:00:19 -0300

    Add /signup-intive route in view.controller

--- server/view/view.controller.ts
+++ server/view/view.controller.ts
@@ -15,9 +15,7 @@ import { ApiTags } from "@nestjs/swagger";
 
 @Controller("/")
 export class ViewController {
-    constructor(
-        private viewService: ViewService,
-    ) { }
+    constructor(private viewService: ViewService) {}
 
     async handler(req: Request, res: Response) {
         const parsedUrl = parse(req.url, true);
@@ -66,6 +64,18 @@ export class ViewController {
         // The person parameter will be used to fetch specific person data
         res.redirect(302, "/about");
 
+    }
+
+    @Get("signup-invite")
+    @Header("Cache-Control", "max-age=86400")
+    public async signupInvite(@Req() req: Request, @Res() res: Response) {
+        const parsedUrl = parse(req.url, true);
+        await this.viewService.render(
+            req,
+            res,
+            "/signup-invite",
+            parsedUrl.query
+        );
     }
 
     @IsPublic()
