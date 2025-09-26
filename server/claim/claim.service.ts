4bf1b1ee7994d08a1ff41aa7d41725cb6e3a70ae
Eduardo <neves98@outlook.com>
2025-09-26 19:48:59 -0300

    bump: update response of service

--- server/claim/claim.service.ts
+++ server/claim/claim.service.ts
@@ -16,6 +16,7 @@ import { ReviewTaskService } from "../review-task/review-task.service";
 import { UtilService } from "../util";
 import { NameSpaceEnum } from "../auth/name-space/schemas/name-space.schema";
 import { GroupService } from "../group/group.service";
+import { ClaimRevision } from "./claim-revision/schema/claim-revision.schema";
 
 type ClaimMatchParameters = (
     | { _id: string; isHidden?: boolean; nameSpace?: string }
@@ -27,6 +28,21 @@ type ClaimMatchParameters = (
       }
 ) &
     FilterQuery<ClaimDocument>;
+
+interface SaveClaimResult {
+    _id: Types.ObjectId;
+    title: string;
+    slug: string;
+    contentId: Types.ObjectId;
+    contentModel: ContentModelEnum;
+    date: Date;
+    claimId: Types.ObjectId;
+    personalities: Types.ObjectId[];
+    latestRevision: Types.ObjectId;
+    isHidden: boolean;
+    nameSpace: string;
+    group?: Types.ObjectId;
+}
 
 @Injectable({ scope: Scope.REQUEST })
 export class ClaimService {
@@ -123,7 +139,7 @@ export class ClaimService {
      * @param claim ClaimBody received of the client.
      * @returns Return a new claim object.
      */
-    async create(claim) {
+    async create(claim): Promise<SaveClaimResult> {
         claim.personalities = claim.personalities.map((personality) => {
             return new Types.ObjectId(personality);
         });
