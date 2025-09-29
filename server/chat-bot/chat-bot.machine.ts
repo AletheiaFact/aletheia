80751fd661e61ca68a7d9b7851ee6b94d7cb16c1
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-09-29 18:34:31 -0300

    changing source type from string to array

--- server/chat-bot/chat-bot.machine.ts
+++ server/chat-bot/chat-bot.machine.ts
@@ -5,7 +5,7 @@ import { VerificationRequestService } from "../verification-request/verification
 export interface ChatBotContext {
     verificationRequest: string;
     responseMessage: string;
-    source?: string;
+    source?: string[];
     publicationDate?: string;
     heardFrom?: string;
     email?: string;
