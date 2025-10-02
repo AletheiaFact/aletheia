8f0998f40fa13866f3d9ce097e8eee954da73f5e
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-10-02 17:53:52 -0300

    passing array value in verification request form

--- src/components/VerificationRequest/CreateVerificationRequest/DynamicVerificationRequestForm.tsx
+++ src/components/VerificationRequest/CreateVerificationRequest/DynamicVerificationRequestForm.tsx
@@ -29,9 +29,7 @@ const DynamicVerificationRequestForm = () => {
         const newVerificationRequest = {
             nameSpace,
             content: data.content,
-            source: Array.isArray(data.source)
-                ? data.source.map((s) => ({ href: s }))
-                : [{ href: data.source }],
+            source: [{ href: data.source }],
             publicationDate: data.publicationDate,
             email: data.email,
             date: new Date(),
