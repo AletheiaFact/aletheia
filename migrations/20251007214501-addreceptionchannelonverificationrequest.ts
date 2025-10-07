4c6003bc4fe8236fa58b68b313917ebdfde4ddf5
LuizFNJ <luizfnj3103@gmail.com>
2025-10-07 21:53:03 +0200

    Migration to add receptionChannel in verification request

---
+++ migrations/20251007214501-addreceptionchannelonverificationrequest.ts
@@ -0,0 +1,11 @@
+import { Db } from "mongodb";
+
+export async function up(db: Db) {
+
+  await db
+    .collection("verificationrequests")
+    .updateMany(
+      {},
+      { $set: { receptionChannel: "instagram" } },
+    );
+}
