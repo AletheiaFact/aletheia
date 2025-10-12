d721e5215d6e05a26c1670769fe48ed117ccee81
caneppelevitor <vitor.a.caneppele@gmail.com>
2025-10-12 14:10:55 +0200

    feat: M2M authentication for chatbot hook and deletion of deprecated middleware

--- server/chat-bot/chat-bot.controller.ts
+++ server/chat-bot/chat-bot.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Post, Req, Res } from "@nestjs/common";
 import { ChatbotService } from "./chat-bot.service";
 import type { Request, Response } from "express";
+import { M2MOrAbilities } from "../auth/decorators/m2m-or-abilities.decorator";
+import { INTEGRATION_ABILITY } from "../auth/ability/abilities.constants";
 
 @Controller()
 export class ChatbotController {
     constructor(private readonly chatBotService: ChatbotService) {}
 
+    @M2MOrAbilities(INTEGRATION_ABILITY)
     @Post("api/chatbot/hook")
     handleHook(@Req() req: Request, @Res() res: Response) {
         const response = this.chatBotService.sendMessage(req.body.message);
--- server/chat-bot/chat-bot.module.ts
+++ server/chat-bot/chat-bot.module.ts
@@ -1,17 +1,12 @@
-import {
-    MiddlewareConsumer,
-    Module,
-    NestModule,
-    RequestMethod,
-} from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { ChatbotService } from "./chat-bot.service";
 import { ChatbotController } from "./chat-bot.controller";
 import { HttpModule } from "@nestjs/axios";
 import { VerificationRequestModule } from "../verification-request/verification-request.module";
 import { ConfigModule } from "@nestjs/config";
-import { AuthZenviaWebHookMiddleware } from "../middleware/auth-zenvia-webhook.middleware";
 import { ChatBotStateModule } from "../chat-bot-state/chat-bot-state.module";
 import { VerificationRequestStateMachineService } from "../verification-request/state-machine/verification-request.state-machine.service";
+import { AbilityModule } from "../auth/ability/ability.module";
 
 @Module({
     imports: [
@@ -19,15 +14,9 @@ import { VerificationRequestStateMachineService } from "../verification-request/
         VerificationRequestModule,
         ConfigModule,
         ChatBotStateModule,
+        AbilityModule,
     ],
     providers: [ChatbotService, VerificationRequestStateMachineService],
     controllers: [ChatbotController],
 })
-export class ChatbotModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
-        consumer.apply(AuthZenviaWebHookMiddleware).forRoutes({
-            path: "api/chatbot/hook",
-            method: RequestMethod.POST,
-        });
-    }
-}
+export class ChatbotModule {}
