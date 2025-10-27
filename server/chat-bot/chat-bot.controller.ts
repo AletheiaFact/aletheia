7352fe5e9551d9bf2aab1e3fb3f1a63a902f17b5
caneppelevitor <vitor.a.caneppele@gmail.com>
2025-10-27 14:46:22 +0100

    refactor: new auth decorator

--- server/chat-bot/chat-bot.controller.ts
+++ server/chat-bot/chat-bot.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Post, Req, Res } from "@nestjs/common";
 import { ChatbotService } from "./chat-bot.service";
 import type { Request, Response } from "express";
-import { M2MOrAbilities } from "../auth/decorators/m2m-or-abilities.decorator";
-import { INTEGRATION_ABILITY } from "../auth/ability/abilities.constants";
+import { AdminOnly } from "auth/decorators/auth.decorator";
 
 @Controller()
 export class ChatbotController {
     constructor(private readonly chatBotService: ChatbotService) {}
 
-    @M2MOrAbilities(INTEGRATION_ABILITY)
+    @AdminOnly({ allowM2M: true })
     @Post("api/chatbot/hook")
     handleHook(@Req() req: Request, @Res() res: Response) {
         const response = this.chatBotService.sendMessage(req.body.message);
