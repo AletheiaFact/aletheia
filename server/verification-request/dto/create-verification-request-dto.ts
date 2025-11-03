017446e41d59245b632699e6130c2a60d1c7fefe
caneppelevitor <vitor.a.caneppele@gmail.com>
2025-11-03 17:43:48 +0100

    fix: removing extra sourceChannel enum

--- server/verification-request/dto/create-verification-request-dto.ts
+++ server/verification-request/dto/create-verification-request-dto.ts
@@ -14,10 +14,6 @@ export class CreateVerificationRequestDTO {
 
     @IsEnum(ContentModelEnum)
     @ApiProperty()
-    sourceChannel: string;
-
-    @IsEnum(ContentModelEnum)
-    @ApiProperty()
     @IsOptional()
     reportType: ContentModelEnum;
 
