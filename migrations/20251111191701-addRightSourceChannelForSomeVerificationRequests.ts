aad1cd49e414e846b0154870962427e5234b3872
LuizFNJ <luizfnj3103@gmail.com>
2025-11-11 19:18:36 +0100

    Fixing unnecessary migrations

--- migrations/20251111191701-addRightSourceChannelForSomeVerificationRequests.ts
+++ migrations/20251111191701-addRightSourceChannelForSomeVerificationRequests.ts
@@ -1,8 +1,14 @@
 import { Db } from "mongodb";
 
 export async function up(db: Db) {
-  const filter = { heardFrom: { $regex: /^Automated Monitoring -/i } };
-  const update = { $set: { sourceChannel: "automated_monitoring" } };
+  const filter = {
+    heardFrom: { $regex: /^Automated Monitoring -/i },
+    sourceChannel: { $ne: "automated_monitoring" },
+  };
+
+  const update = {
+    $set: { sourceChannel: "automated_monitoring" },
+  };
 
   const result = await db
     .collection("verificationrequests")
