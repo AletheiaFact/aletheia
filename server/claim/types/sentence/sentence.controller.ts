f15e233d833244dd8ffaf171291d83843a03055e
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-11-24 10:23:46 -0300

    refactor(auth): replace deprecated IsPublic with Auth({ public: true })

--- server/claim/types/sentence/sentence.controller.ts
+++ server/claim/types/sentence/sentence.controller.ts
@@ -1,29 +1,29 @@
 import { Controller, Param, Put, Body, Get } from "@nestjs/common";
 import { SentenceService } from "./sentence.service";
 import { ApiTags } from "@nestjs/swagger";
-import { IsPublic } from "../../../auth/decorators/is-public.decorator";
 import type { Cop30Sentence } from "../../../../src/types/Cop30Sentence";
 import type { Cop30Stats } from "../../../../src/types/Cop30Stats";
+import { Auth } from "../../../auth/decorators/auth.decorator";
 
 @Controller()
 export class SentenceController {
     constructor(private sentenceService: SentenceService) {}
 
     @ApiTags("claim")
-    @IsPublic()
+    @Auth({ public: true })
     @Get("api/sentence/:data_hash")
     getSentenceByHash(@Param("data_hash") data_hash) {
         return this.sentenceService.getByDataHash(data_hash);
     }
 
-    @IsPublic()
+    @Auth({ public: true })
     @ApiTags("claim")
     @Get("api/sentences/cop30")
     async getAllSentencesWithCop30Topic(): Promise<Cop30Sentence[]> {
         return this.sentenceService.getSentencesByTopic();
     }
 
-    @IsPublic()
+    @Auth({ public: true })
     @ApiTags("claim")
     @Get("api/sentences/cop30/stats")
     async getCop30Stats(): Promise<Cop30Stats> {
