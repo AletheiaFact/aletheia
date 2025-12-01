da0fafc1d0c903c2e592f91cb14895cedef92b70
Eduardo Neves <neves98@outlook.com>
2025-12-01 11:32:50 +0000

    Revert "Merge pull request #2142 from AletheiaFact/feature/cop30-statistics-e…"
    This reverts commit 36a3d2cd95dc79028b4d6be21f5fcb6feb09ea7a.

--- src/types/Cop30Sentence.ts
+++
@@ -1,7 +0,0 @@
-import { Sentence } from "./Sentence";
-import { Review } from "./Review";
-
-export interface Cop30Sentence extends Sentence {
-    classification?: string;
-    review?: Review;
-}
--- src/types/Topic.ts
+++
@@ -1,7 +0,0 @@
-export interface Topic {
-    _id: string;
-    name: string;
-    wikidataId: string;
-    slug: string;
-    language: string;
-}
