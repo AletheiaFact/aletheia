374fe1859a2b7a5b57e292f8d7d17951adeefd3b
LuizFNJ <luizfnj3103@gmail.com>
2026-02-23 22:19:16 +0100

    fix comments

--- cypress/e2e/tests/verificationRequest.cy.ts
+++ cypress/e2e/tests/verificationRequest.cy.ts
@@ -1,7 +1,7 @@
 
 /// <reference types="cypress" />
 
-import { fullVerificationRequest, regexVerificationRequestPage, updatedSource, minimumVerificationRequest } from "../../fixtures/verificationRequest";
+import { fullVerificationRequest, regexVerificationRequestPage, updatedSource, minimumContent } from "../../fixtures/verificationRequest";
 import locators from "../../support/locators";
 import dayjs, { Dayjs } from "dayjs"
 
@@ -10,6 +10,7 @@ describe("Test verification request", () => {
     const getPastDay = (daysAgo: number) => {
         return dayjs().subtract(daysAgo, "day");
     };
+    const getHashFromUrl = (interception) => interception.request.url.split("/").pop();
 
     const openCreateVerificationRequestForm = () => {
         cy.get(locators.floatButton.FLOAT_BUTTON).click();
@@ -35,58 +36,12 @@ describe("Test verification request", () => {
 
     const goToVerificationRequest = (data_hash: string) => {
         cy.intercept("GET", "**/verification-request/**").as("getVerification");
-        cy.visit(`http://localhost:3000/verification-request/${data_hash}`);
+        cy.visit(`/verification-request/${data_hash}`);
         cy.wait("@getVerification");
     }
 
     describe("lifecycle verification request", () => {
         beforeEach("login", () => cy.login());
-
-        it("should create a verification request with all optional and mandatory fields", () => {
-            openCreateVerificationRequestForm();
-
-            cy.get(locators.verificationRequest.FORM_CONTENT).type(fullVerificationRequest.content);
-            selectPublicationDate(today);
-            cy.get(locators.verificationRequest.FORM_REPORT_TYPE).click();
-            cy.contains(fullVerificationRequest.reportType).click();
-            cy.get(locators.verificationRequest.FORM_IMPACT_AREA).type(fullVerificationRequest.impactArea, { delay: 200 });
-            cy.contains(fullVerificationRequest.impactArea).click();
-
-            cy.get(locators.verificationRequest.FORM_HEARD_FROM).type(fullVerificationRequest.heardFrom);
-            cy.get(locators.verificationRequest.FORM_SOURCE).type(`https://${fullVerificationRequest.source}`);
-            cy.get(locators.verificationRequest.FORM_EMAIL).type(fullVerificationRequest.email);
-            cy.intercept("GET", "**/verification-request/**").as("getVerification");
-            saveVerificationRequest();
-            cy.wait("@getVerification");
-            cy.url().should("match", regexVerificationRequestPage);
-
-            assertDetailFields([
-                [locators.verificationRequest.DETAIL_CONTENT, fullVerificationRequest.content],
-                [locators.verificationRequest.DETAIL_REPORT_TYPE, fullVerificationRequest.reportType],
-                [locators.verificationRequest.DETAIL_IMPACT_AREA, fullVerificationRequest.impactArea],
-                [locators.verificationRequest.DETAIL_SOURCE_CHANNEL, "Web"],
-                [locators.verificationRequest.DETAIL_HEARD_FROM, fullVerificationRequest.heardFrom],
-                [locators.verificationRequest.DETAIL_PUBLICATION_DATE, today.format("DD/MM/YYYY")],
-                [locators.verificationRequest.DETAIL_DATE, today.format("DD/MM/YYYY")],
-                [locators.verificationRequest.DETAIL_SOURCE_0, fullVerificationRequest.source],
-            ]);
-        });
-
-        it("should update an existing request by adding additional sources and modifying the publication date", () => {
-            goToVerificationRequest(fullVerificationRequest.data_hash)
-
-            cy.get(locators.verificationRequest.EDIT_BUTTON).should("be.visible").click();
-            cy.get(locators.verificationRequest.FORM_SOURCE_ADD).click();
-            cy.get(locators.verificationRequest.FORM_SOURCE_ITEM_1).type(`https://${updatedSource}`);
-            selectPublicationDate(getPastDay(1));
-            saveVerificationRequest();
-            cy.url().should("match", regexVerificationRequestPage);
-
-            assertDetailFields([
-                [locators.verificationRequest.DETAIL_PUBLICATION_DATE, getPastDay(1).format("DD/MM/YYYY")],
-                [locators.verificationRequest.DETAIL_SOURCE_1, updatedSource],
-            ])
-        })
 
         it("should prevent submission when required fields are missing", () => {
             openCreateVerificationRequestForm();
@@ -96,64 +51,135 @@ describe("Test verification request", () => {
             cy.url().should("contain", "/verification-request/create");
         });
 
-        it("should allow request creation using only the minimum mandatory information", () => {
-            openCreateVerificationRequestForm();
+        describe("Full verification request flow", () => {
+            let fullRequestHash: string;
 
-            cy.get(locators.verificationRequest.FORM_CONTENT).type(minimumVerificationRequest.content);
-            selectPublicationDate(today);
-            cy.intercept("GET", "**/verification-request/**").as("getVerification");
-            saveVerificationRequest();
-            cy.wait("@getVerification");
-            cy.url().should("match", regexVerificationRequestPage);
+            it("should create a verification request with all optional and mandatory fields", () => {
+                openCreateVerificationRequestForm();
+                cy.intercept("GET", "**/verification-request/**").as("getVerification");
 
-            assertDetailFields([
-                [locators.verificationRequest.DETAIL_CONTENT, minimumVerificationRequest.content],
-                [locators.verificationRequest.DETAIL_PUBLICATION_DATE, today.format("DD/MM/YYYY")],
-            ])
-        });
+                cy.get(locators.verificationRequest.FORM_CONTENT).type(fullVerificationRequest.content);
+                selectPublicationDate(today);
+                cy.get(locators.verificationRequest.FORM_REPORT_TYPE).click();
+                cy.contains(fullVerificationRequest.reportType).click();
+                cy.get(locators.verificationRequest.FORM_IMPACT_AREA).type(fullVerificationRequest.impactArea, { delay: 200 });
+                cy.contains(fullVerificationRequest.impactArea).click();
 
-        it("should discard unsaved changes when the edition form is canceled", () => {
-            goToVerificationRequest(minimumVerificationRequest.data_hash)
+                cy.get(locators.verificationRequest.FORM_HEARD_FROM).type(fullVerificationRequest.heardFrom);
+                cy.get(locators.verificationRequest.FORM_SOURCE).type(`https://${fullVerificationRequest.source}`);
+                cy.get(locators.verificationRequest.FORM_EMAIL).type(fullVerificationRequest.email);
+                saveVerificationRequest();
 
-            cy.get(locators.verificationRequest.EDIT_BUTTON).should("be.visible").click();
-            cy.get(locators.verificationRequest.FORM_SOURCE_ITEM_0).type(`https://${fullVerificationRequest.source}`);
-            selectPublicationDate(getPastDay(10));
-            cy.get(locators.verificationRequest.CANCEL_BUTTON).click();
+                cy.wait("@getVerification").then((interception) => {
+                    getHashFromUrl(interception)
+                    fullRequestHash = getHashFromUrl(interception);
+                    cy.log("Hash capturado:", fullRequestHash);
+                });
+                cy.url().should("match", regexVerificationRequestPage);
 
-            cy.get(locators.verificationRequest.DETAIL_PUBLICATION_DATE).should("be.visible").and("not.contain", getPastDay(10).format("DD/MM/YYYY"));
-            cy.get(locators.verificationRequest.DETAIL_SOURCE_0).should("not.exist");
-        })
+                assertDetailFields([
+                    [locators.verificationRequest.DETAIL_CONTENT, fullVerificationRequest.content],
+                    [locators.verificationRequest.DETAIL_REPORT_TYPE, fullVerificationRequest.reportType],
+                    [locators.verificationRequest.DETAIL_IMPACT_AREA, fullVerificationRequest.impactArea],
+                    [locators.verificationRequest.DETAIL_SOURCE_CHANNEL, "Web"],
+                    [locators.verificationRequest.DETAIL_HEARD_FROM, fullVerificationRequest.heardFrom],
+                    [locators.verificationRequest.DETAIL_PUBLICATION_DATE, today.format("DD/MM/YYYY")],
+                    [locators.verificationRequest.DETAIL_DATE, today.format("DD/MM/YYYY")],
+                    [locators.verificationRequest.DETAIL_SOURCE_0, fullVerificationRequest.source],
+                ]);
 
-        it("should supplement a minimalist request by adding its first source during edition", () => {
-            goToVerificationRequest(minimumVerificationRequest.data_hash)
+                it("should update an existing request by adding additional sources and modifying the publication date", () => {
+                    goToVerificationRequest(fullRequestHash)
 
-            cy.get(locators.verificationRequest.EDIT_BUTTON).should("be.visible").click();
-            cy.get(locators.verificationRequest.FORM_SOURCE_ITEM_0).type(`https://${fullVerificationRequest.source}`);
-            selectPublicationDate(getPastDay(1));
-            saveVerificationRequest();
-            cy.url().should("match", regexVerificationRequestPage);
+                    cy.get(locators.verificationRequest.EDIT_BUTTON).should("be.visible").click();
+                    cy.get(locators.verificationRequest.FORM_SOURCE_ADD).click();
+                    cy.get(locators.verificationRequest.FORM_SOURCE_ITEM_1).type(`https://${updatedSource}`);
+                    selectPublicationDate(getPastDay(1));
+                    saveVerificationRequest();
+                    cy.url().should("match", regexVerificationRequestPage);
 
-            assertDetailFields([
-                [locators.verificationRequest.DETAIL_PUBLICATION_DATE, getPastDay(1).format("DD/MM/YYYY")],
-                [locators.verificationRequest.DETAIL_SOURCE_0, fullVerificationRequest.source],
-            ])
-        })
+                    assertDetailFields([
+                        [locators.verificationRequest.DETAIL_PUBLICATION_DATE, getPastDay(1).format("DD/MM/YYYY")],
+                        [locators.verificationRequest.DETAIL_SOURCE_1, updatedSource],
+                    ])
+                })
+            })
 
-        it("should manage topic tags by adding and removing them", () => {
-            goToVerificationRequest(minimumVerificationRequest.data_hash)
+            it("should manage topic tags by adding and removing them", () => {
+                goToVerificationRequest(fullRequestHash)
+                cy.intercept("PUT", "**/verification-request/*/topics").as("updateTopics");
 
-            cy.get(locators.verificationRequest.ADD_TOPIC_ICON).click();
-            cy.get(locators.verificationRequest.TYPE_TOPIC_INPUT).type(fullVerificationRequest.impactArea.toLowerCase(), { delay: 200 });
-            cy.contains(fullVerificationRequest.impactArea).click();
-            cy.get(locators.verificationRequest.ADD_TOPIC_SUBMIT).click();
+                cy.get(locators.verificationRequest.ADD_TOPIC_ICON).click();
+                cy.get(locators.verificationRequest.TYPE_TOPIC_INPUT).type(fullVerificationRequest.topic, { delay: 200 });
+                cy.contains(fullVerificationRequest.topic).click();
+                cy.get(locators.verificationRequest.ADD_TOPIC_SUBMIT).click();
 
-            cy.contains(locators.verificationRequest.DETAIL_TOPIC_TAG, fullVerificationRequest.impactArea.toUpperCase())
-                .should("be.visible")
-                .within(() => {
-                    cy.get(locators.verificationRequest.REMOVE_TOPIC_ICON).click();
+                cy.wait("@updateTopics").then((interception) => {
+                    expect(interception.response.statusCode).to.be.oneOf([200, 201]);
                 });
 
-            cy.contains(locators.verificationRequest.DETAIL_TOPIC_TAG, fullVerificationRequest.impactArea.toUpperCase()).should("not.exist");
+                cy.contains(locators.verificationRequest.DETAIL_TOPIC_TAG, fullVerificationRequest.topic.toUpperCase())
+                    .should("be.visible")
+                    .within(() => {
+                        cy.get(locators.verificationRequest.REMOVE_TOPIC_ICON).click();
+                    });
+
+
+                cy.contains(locators.verificationRequest.DETAIL_TOPIC_TAG, fullVerificationRequest.topic.toUpperCase()).should("not.exist");
+            })
+
+            it("should discard unsaved changes when the edition form is canceled", () => {
+                goToVerificationRequest(fullRequestHash)
+
+                cy.get(locators.verificationRequest.EDIT_BUTTON).should("be.visible").click();
+                cy.get(locators.verificationRequest.FORM_SOURCE_ADD).click();
+                cy.get(locators.verificationRequest.FORM_SOURCE_ITEM_1).type(`https://${updatedSource}`);
+                selectPublicationDate(getPastDay(10));
+                cy.get(locators.verificationRequest.CANCEL_BUTTON).click();
+
+                cy.get(locators.verificationRequest.DETAIL_PUBLICATION_DATE).should("be.visible").and("not.contain", getPastDay(10).format("DD/MM/YYYY"));
+                cy.get(locators.verificationRequest.DETAIL_SOURCE_1).should("not.exist");
+            })
+        });
+
+        describe("Minimum verification request flow", () => {
+            let minRequestHash: string;
+
+            it("should allow request creation using only the minimum mandatory information", () => {
+                openCreateVerificationRequestForm();
+                cy.intercept("GET", "**/verification-request/**").as("getVerification");
+
+                cy.get(locators.verificationRequest.FORM_CONTENT).type(minimumContent);
+                selectPublicationDate(today);
+                saveVerificationRequest();
+                cy.wait("@getVerification").then((interception) => {
+                    getHashFromUrl(interception)
+                    minRequestHash = getHashFromUrl(interception);
+
+                    cy.log("Hash capturado:", minRequestHash);
+                });
+                cy.url().should("match", regexVerificationRequestPage);
+
+                assertDetailFields([
+                    [locators.verificationRequest.DETAIL_CONTENT, minimumContent],
+                    [locators.verificationRequest.DETAIL_PUBLICATION_DATE, today.format("DD/MM/YYYY")],
+                ])
+            });
+
+            it("should supplement a minimalist request by adding its first source during edition", () => {
+                goToVerificationRequest(minRequestHash)
+
+                cy.get(locators.verificationRequest.EDIT_BUTTON).should("be.visible").click();
+                cy.get(locators.verificationRequest.FORM_SOURCE_ITEM_0).type(`https://${fullVerificationRequest.source}`);
+                selectPublicationDate(getPastDay(1));
+                saveVerificationRequest();
+                cy.url().should("match", regexVerificationRequestPage);
+
+                assertDetailFields([
+                    [locators.verificationRequest.DETAIL_PUBLICATION_DATE, getPastDay(1).format("DD/MM/YYYY")],
+                    [locators.verificationRequest.DETAIL_SOURCE_0, fullVerificationRequest.source],
+                ])
+            })
         })
     });
 });
--- cypress/fixtures/verificationRequest.ts
+++ cypress/fixtures/verificationRequest.ts
@@ -2,18 +2,15 @@ const fullVerificationRequest = {
     content: "Verification Request Content",
     reportType: "Discurso",
     impactArea: "Ambientalismo",
+    topic: "Socialismo",
     heardFrom: "Verification Request heardFrom",
     source: "wikimedia.org",
     email: "test-cypress@aletheiafact.org",
-    data_hash: "7908c206338ec42bacb83f256d6b484f",
 };
 
-const minimumVerificationRequest = {
-    content: "Verification Request Content minimium",
-    data_hash: "f2a562ab2f47f8a878cf92326c81fca8"
-}
+const minimumContent = "Verification Request Content minimium"
 
 const regexVerificationRequestPage = /\/verification-request\/[\w-]+$/
 const updatedSource = "www.wikidata.org"
 
-export { fullVerificationRequest, regexVerificationRequestPage, updatedSource, minimumVerificationRequest }
+export { fullVerificationRequest, regexVerificationRequestPage, updatedSource, minimumContent }
