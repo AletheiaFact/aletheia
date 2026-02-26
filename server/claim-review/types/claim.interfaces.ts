34e020e4adb7c5a5573f537d280cd392758ad112
LuizFNJ <luizfnj3103@gmail.com>
2026-02-26 20:37:47 +0100

    refactor: use aggregation pipeline for efficient filtering and add types

---
+++ server/claim-review/types/claim.interfaces.ts
@@ -0,0 +1,58 @@
+import { Sentence } from "claim/types/sentence/schemas/sentence.schema";
+import { User } from "../../entities/user.entity";
+import { Personality } from "../../personality/mongo/schemas/personality.schema";
+import { Report } from "../../report/schemas/report.schema";
+import { Image } from "../../claim/types/image/schemas/image.schema";
+import { ContentModelEnum } from "../../types/enums";
+
+interface claim {
+    contentModel: ContentModelEnum;
+    date: Date;
+    slug: string;
+    title: string;
+    claimId: string;
+}
+
+export interface IListAllQuery {
+    isHidden?: boolean | string;
+    nameSpace?: string;
+    isDeleted?: boolean;
+}
+
+export interface IlistAll {
+    page: number;
+    pageSize: number;
+    order: "asc" | "desc";
+    query: IListAllQuery;
+    latest?: boolean;
+}
+
+
+export interface ClaimReviewAggregated {
+    _id?: string;
+    nameSpace: string;
+    isPartialReview: boolean;
+    isHidden: boolean;
+    isDeleted?: boolean;
+    deletedAt?: Date | null;
+    personality?: Personality
+    usersId: User[];
+    report: Report;
+    target: claim;
+    targetModel: string;
+    data_hash: string;
+    reportModel: string;
+    date: Date;
+    isPublished: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+    __v?: number;
+}
+
+export interface listAllResponse {
+    content: Image | Sentence;
+    personality?: Personality;
+    reviewHref: string;
+    claim: claim;
+    report: Report;
+}
