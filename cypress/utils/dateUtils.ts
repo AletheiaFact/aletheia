b41530e96563a2b83376472dbdc1191c999f0b86
LuizFNJ <luizfnj3103@gmail.com>
2026-03-02 17:33:59 +0100

    feat: add global command to select date in datepicker

--- cypress/utils/dateUtils.ts
+++ cypress/utils/dateUtils.ts
@@ -4,3 +4,6 @@ export const today = dayjs();
 
 export const getPastDay = (daysAgo: number) =>
     dayjs().subtract(daysAgo, "day");
+
+export const getFutureDay = (daysAhead: number) =>
+    dayjs().add(daysAhead, "day");
