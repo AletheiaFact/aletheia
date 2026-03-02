4ebb07eb71999ac339ecf4a5f5c2a118d8ac49a6
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2026-03-02 15:23:26 -0300

    removing temporarily add checked informarion button from the affixbutton and the sourcelist in ClaimListView

--- src/components/Claim/ClaimListView.tsx
+++ src/components/Claim/ClaimListView.tsx
@@ -1,24 +1,26 @@
 import { Grid } from "@mui/material"
 import React from "react";
 import ClaimList from "./ClaimList";
-import SourceList from "../Source/SourceList";
 import { useTranslation } from "react-i18next";
 
 const ClaimListView = () => {
     const { t } = useTranslation();
     return (
         <>
-            <Grid container style={{ marginTop: "64px", justifyContent:"center" }}>
+            <Grid container style={{ marginTop: "64px", justifyContent: "center" }} >
                 <Grid item sm={7} md={7} lg={10.5}>
                     <h1 style={{ fontSize: 32 }}>
                         {t("claim:claimListTitle")}
                     </h1>
                 </Grid>
-                <Grid item sm={7} md={7} lg={6}>
+
+                // The sourcelist has been temporarily removed.
+                {/* <Grid item sm={7} md={7} lg={6}>
                     <SourceList />
-                </Grid>
-                <Grid item style={{ margin: "0 20px" }} sm={7} md={7} lg={4}>
-                    <ClaimList columns={12} personality={{ _id: null }} />
+                </Grid> */}
+
+                <Grid item style={{ margin: "0 20px" }} sm={7} md={7} lg={10.5}>
+                    <ClaimList columns={4} personality={{ _id: null }} />
                 </Grid>
             </Grid>
         </>
