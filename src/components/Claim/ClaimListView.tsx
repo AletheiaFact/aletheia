b162cc16e7a012d4fe555752ebd5c1ff835a9fb7
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2026-03-05 11:40:57 -0300

    Adjusting the grid to be in the same position as the hamburger menu and passing typography.

--- src/components/Claim/ClaimListView.tsx
+++ src/components/Claim/ClaimListView.tsx
@@ -1,8 +1,7 @@
-import { Grid } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import React from "react";
 import ClaimList from "./ClaimList";
 import { useTranslation } from "react-i18next";
-import Paragraph from "../Paragraph";
 // import SourceList from "../Source/SourceList";
 
 const ClaimListView = () => {
@@ -10,11 +9,13 @@ const ClaimListView = () => {
     return (
         <>
             <Grid container style={{ marginTop: "64px", justifyContent: "center" }} >
-                <Grid item sm={7} md={7} lg={7}>
-                    <h1 style={{ fontSize: 32 }}>
+                <Grid item xs={12} md={9.5}>
+                    <Typography variant="h1" fontSize={32}>
                         {t("claim:claimListTitle")}
-                    </h1>
-                    <Paragraph>{t("claim:claimListDescription")}</Paragraph>
+                    </Typography>
+                    <Typography variant="body1">
+                        {t("claim:claimListDescription")}
+                    </Typography>
                 </Grid>
 
                 {/* The sourcelist has been temporarily removed. */}
@@ -22,7 +23,7 @@ const ClaimListView = () => {
                     <SourceList />
                 </Grid> */}
 
-                <Grid item style={{ margin: "0 20px" }} sm={7} md={7} lg={7}>
+                <Grid item style={{ margin: "0 20px" }} xs={12} md={9.5}>
                     <ClaimList columns={6} personality={{ _id: null }} />
                 </Grid>
             </Grid>
