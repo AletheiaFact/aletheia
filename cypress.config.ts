146db05da5265651ad9ecc1d1e4b5407738774c3
LuizFNJ <luizfnj3103@gmail.com>
2026-03-21 01:35:57 +0100

    test(event): define e2e spec execution order in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
         baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:3000",
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
+        specPattern: [
+            "cypress/e2e/tests/**/!(event).cy.ts",
+            "cypress/e2e/tests/event.cy.ts"
+        ],
         setupNodeEvents(on, config) {
             return require("./cypress/plugins/index.ts").default(on, config);
         },
