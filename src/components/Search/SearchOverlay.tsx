78db25fe4f568946d80f927b0d609470a9cd2c5a
LuizFNJ <luizfnj3103@gmail.com>
2026-05-15 14:18:40 +0200

    Fix(Buttons): Fixing error after rebase

--- src/components/Search/SearchOverlay.tsx
+++ src/components/Search/SearchOverlay.tsx
@@ -8,18 +8,18 @@ import { ArrowCircleLeft } from "@mui/icons-material";
 import actions from "../../store/actions";
 import { useAppSelector } from "../../store/store";
 import colors from "../../styles/colors";
-import { queries } from "../../styles/mediaQueries";
 import AletheiaButton, { ButtonType } from "../AletheiaButton";
 import OverlaySearchInput from "./OverlaySearchInput";
 import { NameSpaceEnum } from "../../types/Namespace";
 import { currentNameSpace } from "../../atoms/namespace";
+import { queries } from "../../styles/mediaQueries";
 
 const OverlayWrapper = styled.div`
-  position: fixed;
+  position: absolute;
   top: 0;
   right: 0;
   width: 40vw;
-  height: 64px;
+  height: 60px;
   z-index: 1000;
   background-color: ${({ $namespace }) =>
         $namespace === NameSpaceEnum.Main ? colors.primary : colors.secondary};
