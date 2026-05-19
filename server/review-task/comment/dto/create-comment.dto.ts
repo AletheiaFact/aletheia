2cedb5d96ca14d444e6aa4b28161f1bb920470b6
LuizFNJ <luizfnj3103@gmail.com>
2026-05-19 18:13:55 +0200

    refactor(comment): use type-only import for UserDocument

--- server/review-task/comment/dto/create-comment.dto.ts
+++ server/review-task/comment/dto/create-comment.dto.ts
@@ -6,7 +6,7 @@ import {
     IsOptional,
     IsString,
 } from "class-validator";
-import { UserDocument } from "../../../users/schemas/user.schema";
+import type { UserDocument } from "../../../users/schemas/user.schema";
 import { CommentEnum } from "../schema/comment.schema";
 
 export class CreateCommentDTO {
