2c432d32988826c93a723cd5f4fd7b043e52678f
LuizFNJ <luizfnj3103@gmail.com>
2026-06-18 21:13:04 +0200

    fix(review-card): revert image card height to original

--- src/components/ImageClaim.tsx
+++ src/components/ImageClaim.tsx
@@ -20,8 +20,8 @@ const ImageClaim: React.FC<ImageClaimProps> = ({ src, title = "" }) => {
                 overflow: "hidden",
                 marginTop: "16px",
                 marginBottom: "16px",
-                minHeight: "120px",
-                maxHeight: "200px",
+                minHeight: { xs: "120px", sm: "150px", md: "200px" },
+                maxHeight: { xs: "180px", sm: "250px", md: "320px" },
             }}
         >
             <img
